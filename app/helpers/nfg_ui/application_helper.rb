1f8ac7ec2100f75411202b6598fc261d4acce0a1
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-07-03 16:10:19 -0400

    removes convenience method since it was getting out of sync with the original method.

--- app/helpers/nfg_ui/application_helper.rb
+++ app/helpers/nfg_ui/application_helper.rb
@@ -4,12 +4,5 @@ module NfgUi
   # Exposes nfg_ui rendering method
   module ApplicationHelper
     include NfgUi::Builder
-
-    # Exposed here for convenience -- otherwise #nfg_ui seems
-    # awfully magical and perhaps challenging to track down
-    # in a hurry (if missing what NfgUi::Builder does)
-    def nfg_ui(component_name, trait = nil, **options, &block)
-      super
-    end
   end
 end
