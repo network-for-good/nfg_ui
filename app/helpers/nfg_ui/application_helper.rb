f0539584d6aeea0dbc826ab0844e29a93c2d39f2
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-07-08 22:36:25 -0400

    Enhances tiles to allow for auto-generating footer.

--- app/helpers/nfg_ui/application_helper.rb
+++ app/helpers/nfg_ui/application_helper.rb
@@ -3,7 +3,6 @@
 module NfgUi
   # Core rendering helper methods
   module ApplicationHelper
-
     # render components from the pertinent suite
     # Network for Good components: = ui.nfg
     # Bootstrap core components:   = ui.bootstrap
--- lib/nfg_ui/components/patterns/tile.rb
+++ lib/nfg_ui/components/patterns/tile.rb
@@ -5,12 +5,25 @@ module NfgUi
     module Patterns
       # Tile doc coming soon
       class Tile < Bootstrap::Components::Base
-        attr_reader :icon
+        attr_reader :icon, :title
+
+        def initialize(*)
+          super
+          @title = options.fetch(:title, '')
+          @icon = options.fetch(:icon, '')
+        end
+
+        # Optionally auto-load a footer by passing it in
+        # ex: = ui.nfg :tile, body: 'Example', footer: 'Example footer'
+        def footer
+          options[:footer].presence
+        end
 
         private
 
         def defaults
-          super.merge!(icon: 'circle-o')
+          super.merge!(icon: 'circle-o',
+                       title: '')
         end
 
         def component_family
@@ -18,7 +31,7 @@ module NfgUi
         end
 
         def non_html_attribute_options
-          super.push(:icon)
+          super.push(:icon, :title, (:footer if footer))
         end
       end
     end
