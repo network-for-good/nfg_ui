217f4c99c51b41f32f3b393b8c13a7111d0afad0
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-07-10 07:22:43 -0400

    Establishes a base NFG component

---
+++ lib/nfg_ui/components/base.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module NfgUi
+  module Components
+    # Base NFG UI component
+    # Currently is a class that inherits the bootstrap base component
+    class Base < Bootstrap::Components::Base
+    end
+  end
+end
