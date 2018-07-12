a108121862dc9acd9b73f4d04d0347940006eabc
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-07-11 23:18:53 -0400

    Makes it easier for documentation to communication what set of stylesheets you're looking at.

--- spec/test_app/app/controllers/application_controller.rb
+++ spec/test_app/app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :supply_default_stylesheet_param
+
+  def supply_default_stylesheet_param
+    if controller_path.include?('bootstrap')
+      redirect_to url_for if params[:stylesheet].present?  
+    elsif params[:stylesheet].nil?
+      redirect_to url_for(stylesheet: 'public')
+    end
+  end
 end
