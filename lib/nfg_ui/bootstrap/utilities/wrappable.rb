732c26ca1e828fced8da6d2a7f5b133c032f0e41
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-07-13 16:44:01 -0400

    Fixes wrappable to now provide accurate defaults -- and now correctly sets a default href when a link is present and there is no href in the options hash.

--- lib/nfg_ui/bootstrap/utilities/wrappable.rb
+++ lib/nfg_ui/bootstrap/utilities/wrappable.rb
@@ -8,16 +8,19 @@ module NfgUi
       # is converting :link to :a for tag generation
       module Wrappable
         attr_reader :default_html_wrapper_element
-        attr_writer :as
+        attr_writer :as, :options
 
-        def initialize(*)
+        def initialize(component_options, *)
           super
-          self.as = options.fetch(:as, default_html_wrapper_element) || as_becomes_link
-          wrapper_element_error if default_html_wrapper_element.nil? 
+          self.as = options.fetch(:as, as_becomes_link) || default_html_wrapper_element
+          self.options = defaults.merge!(**default_link, **options) if link?
+
+          default_html_wrapper_element_error if default_html_wrapper_element_error?
+          as_competes_with_href_error if as_competes_with_href?
         end
 
         def html_wrapper_element
-          link? ? :a : as
+          as_link? ? :a : as
         end
 
         private
@@ -30,31 +33,52 @@ module NfgUi
           :a if options.try(:href)
         end
 
-        # UPDATING DEFAULTS FROM LINK_DEFAULTS DOES NOT WORK YET
-        # def defaults
-        #   super.merge!(link_defaults)
-        # end
+        def non_html_attribute_options
+          super.push(:as)
+        end
 
-        # def link_defaults
-        #   link? ? { href: 'javascripts:;' } : {}
-        # end
+        def as_link?
+          as == :link
+        end
 
-        # Set this as necessary on components using the Wrappable module.
-        def wrapper_element_error
+        def link?
+          html_wrapper_element == :a
+        end
+
+        def default_link
+          { href: 'javascript:;' }
+        end
+
+        def as_competes_with_href?
+          (options[:href].present? && !link?)
+        end
+
+        def as_competes_with_href_error
+          raise ArgumentError.new "#{self.class.name} passed in :#{as} but also passed in an :href. Only pass in an :href if you're using a link. You may pass in an href without an :as option to generate a link.
+Example of incorrect usage (note that this attempts to inject an href into a <button>):
+= ui.nfg :button, as: :button, href: 'http://google.com', body: 'Button'
+
+Example of correct usage:
+= ui.nfg :button, body: 'Button', href: 'http://google.com'
+
+or, explicitly state that this component is a link
+
+= ui.nfg :button, as: :a, body: 'Button', href: 'http://google.com'
+= ui.nfg :button, as: :link, body: 'Button', href: 'http://google.com'
+          "
+        end
+
+        def default_html_wrapper_element_error?
+          default_html_wrapper_element.nil?
+        end
+
+        def default_html_wrapper_element_error
           raise ArgumentError.new "#{self.class.name} is expected to define a #default_html_wrapper_element\n\nExample:\n
 class #{self.class.name} < Bootstrap::Components::Base
   def default_html_wrapper_element
     :a
   end
 end"
-        end
-
-        def non_html_attribute_options
-          super.push(:as)
-        end
-
-        def link?
-          as == :link
         end
       end
     end
