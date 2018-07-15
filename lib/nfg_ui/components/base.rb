4afc3a299877692453a85f9090ea5e1395423cb1
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-07-14 22:22:21 -0400

    Removes autoloading the traits because... many of the bootstrap inherited components never get this method sent to them. I don't want to put this NFG UI component code into the BS suite.

--- lib/nfg_ui/components/base.rb
+++ lib/nfg_ui/components/base.rb
@@ -2,34 +2,10 @@
 
 module NfgUi
   module Components
-    # Base NFG UI component
-    # Currently is a class that inherits the bootstrap base component 
-    # because it provides everything needed for now.
+    # Base NFG UI component for compnoents that are not directly
+    # inherited from Bootstrap base components (e.g.: buttons do not
+    # inherit this Base class, but Tile does)
     class Base < Bootstrap::Components::Base
-      # Trait modules are automatically included
-      # assuming the ancestry / naming convention is followed.
-      #
-      # Example:
-      # NfgUi::Components::Foundations::Typeface   <-- Component
-      # NfgUi::Components::Traits::Typeface        <-- Trait
-      def initialize(component_options, view_context)
-        include_trait_module if include_trait_module?
-        super
-      end
-
-      private
-
-      def include_trait_module?
-        NfgUi::Components::Traits.const_defined?(class_name)
-      end
-
-      def include_trait_module
-        singleton_class.send(:include, NfgUi::Components::Traits.const_get(class_name))
-      end
-
-      def class_name
-        self.class.name.demodulize
-      end
     end
   end
 end
