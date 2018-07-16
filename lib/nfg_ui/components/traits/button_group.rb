4ceb9fa0ea209a48bffd91b98b620f6566d54d83
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-07-15 23:01:16 -0400

    Adds button group traits

---
+++ lib/nfg_ui/components/traits/button_group.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module NfgUi
+  module Components
+    module Traits
+      # Access to pre-designed traits
+      module ButtonGroup
+        attr_reader :page_header_toolbar
+
+        def initialize(*)
+          super
+          @page_header_toolbar = traits.include?(:page_header_toolbar)
+        end
+
+        def toolbar_html_options
+          page_header_toolbar ? super.merge(class: 'float-lg-right') : super
+        end
+
+        private
+
+        def sticky_div_data_attributes
+          { toggle: 'sticky-div' }
+        end
+      end
+    end
+  end
+end
