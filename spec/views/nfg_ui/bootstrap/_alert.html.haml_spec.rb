4e683bfb1739aa2620218cc1c24bf84d02d633a2
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-07-31 16:34:18 -0400

    adds alert spec.

--- spec/views/nfg_ui/bootstrap/_alert.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/_alert.html.haml_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'nfg_ui/bootstrap/_alert.html.haml', type: :view do
-  let(:alert) { FactoryBot.create(:bootstrap_alert) }
-  subject { render partial: 'nfg_ui/bootstrap/alert', alert: alert }
-  # subject { view_context.ui.nfg :alert }
+  let(:body) { 'test body content' }
+  let(:alert) { FactoryBot.create(:bootstrap_alert, body: body) }
+  subject { render 'nfg_ui/bootstrap/alert', alert: alert }
 
-  it { raise alert.inspect }
+  it 'outputs a bootstrap alert component in html' do
+    expect(subject).to have_css '.alert'
+    expect(subject).to have_css ".alert[role='alert']"
+    expect(subject).to have_selector '.alert', text: body
+  end
 end
