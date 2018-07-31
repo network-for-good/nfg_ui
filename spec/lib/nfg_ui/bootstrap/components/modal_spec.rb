4fecff1c60703fdbf9b30d39885c8552512f465a
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-07-31 15:30:34 -0400

    Adds initial specs for bootstrap components.

---
+++ spec/lib/nfg_ui/bootstrap/components/modal_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe NfgUi::Bootstrap::Components::Modal do
+  subject { described_class }
+  it { is_expected.to be < NfgUi::Bootstrap::Components::Base }
+end
---
+++ spec/lib/nfg_ui/components/utilities/titleable_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe NfgUi::Components::Utilities::Titleable do
+  pending 'coming soon'
+end
