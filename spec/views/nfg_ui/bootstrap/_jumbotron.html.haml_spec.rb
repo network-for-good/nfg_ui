5c11c9385a5288c9f3a836eb4f5f9c45281033ea
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-08-01 07:39:17 -0400

    adds jumbotron spec.

--- spec/views/nfg_ui/bootstrap/_jumbotron.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/_jumbotron.html.haml_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe 'nfg_ui/bootstrap/_jumbotron.html.haml', type: :view do
   let(:jumbotron) { FactoryBot.create(:bootstrap_jumbotron, body: body) }
   subject { render 'nfg_ui/bootstrap/jumbotron', jumbotron: jumbotron }
 
-  pending 'coming soon'
-
   it 'outputs a bootstrap jumbotron component in html' do
+    expect(subject).to have_css '.jumbotron'
+    expect(subject).to have_selector '.jumbotron', text: body
+    expect(subject).to eq "<div class='jumbotron'>\n#{body}\n</div>\n"
   end
 end
