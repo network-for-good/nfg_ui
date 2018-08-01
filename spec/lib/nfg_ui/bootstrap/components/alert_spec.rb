2f57f67fb6525186f690da12a861be7293afd25a
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-08-01 17:17:01 -0400

    bolsters alert class spec.

--- spec/lib/nfg_ui/bootstrap/components/alert_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/alert_spec.rb
@@ -6,20 +6,31 @@ RSpec.describe NfgUi::Bootstrap::Components::Alert do
 
   it { expect(described_class).to be < NfgUi::Bootstrap::Components::Base }
 
+  describe 'default class construction' do
+    let(:options) { {} }
+    it 'contains only the public methods and options it is expected to' do
+      expect(alert.options).to eq(class: '', id: nil, body: nil, data: nil, traits: [])
+      expect(alert.view_context).to be
+      expect(alert.id).to be_nil
+      expect(alert.data).to be_nil
+      expect(alert.body).to be_nil
+    end
+  end
+
   describe '#assistive_html_attributes' do
     subject { alert.send(:assistive_html_attributes) }
     it { is_expected.to eq(role: 'alert') }
   end
 
   describe 'Dismissible module included in Alert' do
-    it { expect(described_class).to include NfgUi::Bootstrap::Utilities::Dismissible }
+    it { expect(described_class.included_modules).to include NfgUi::Bootstrap::Utilities::Dismissible }
     it 'responds to the dismissible public methods' do
       expect(alert).to respond_to :dismissible, :dismissible?
     end
   end
 
   describe 'Themeable module included in Alert' do
-    it { expect(described_class).to include NfgUi::Bootstrap::Utilities::Themeable }
+    it { expect(described_class.included_modules).to include NfgUi::Bootstrap::Utilities::Themeable }
     it 'responds to the themeable public methods' do
       expect(alert).to respond_to :theme, :outlined
     end
