50811d3a02d0b4b6bf8824aa7661e64e585b2a4a
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-07-31 22:12:34 -0400

    Adds factories for remaining bootstrap components

--- spec/factories/bootstrap.rb
+++ spec/factories/bootstrap.rb
@@ -11,7 +11,8 @@ FactoryBot.define do
     initialize_with { new(attributes, ActionController::Base.new.view_context) }
 
     trait :rendered do
-      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context).bootstrap(:alert, attributes) }
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:alert, attributes) }
     end
   end
 
@@ -26,7 +27,8 @@ FactoryBot.define do
     initialize_with { new(attributes, ActionController::Base.new.view_context) }
 
     trait :rendered do
-      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context).bootstrap(:badge, attributes) }
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:badge, attributes) }
     end
   end
 
@@ -40,7 +42,8 @@ FactoryBot.define do
     initialize_with { new(attributes, ActionController::Base.new.view_context) }
 
     trait :rendered do
-      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context).bootstrap(:breadcrumb, attributes) }
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:breadcrumb, attributes) }
     end
   end
 
@@ -54,7 +57,8 @@ FactoryBot.define do
     initialize_with { new(attributes, ActionController::Base.new.view_context) }
 
     trait :rendered do
-      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context).bootstrap(:breadcrumb_item, attributes) }
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:breadcrumb_item, attributes) }
     end
   end
 
@@ -68,7 +72,8 @@ FactoryBot.define do
     initialize_with { new(attributes, ActionController::Base.new.view_context) }
 
     trait :rendered do
-      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context).bootstrap(:button_group, attributes) }
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:button_group, attributes) }
     end
   end
 
@@ -82,7 +87,23 @@ FactoryBot.define do
     initialize_with { new(attributes, ActionController::Base.new.view_context) }
 
     trait :rendered do
-      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context).bootstrap(:button_toolbar, attributes) }
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:button_toolbar, attributes) }
+    end
+  end
+
+  factory :bootstrap_card, class: NfgUi::Bootstrap::Components::Card do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:card, attributes) }
     end
   end
 
@@ -96,7 +117,8 @@ FactoryBot.define do
     initialize_with { new(attributes, ActionController::Base.new.view_context) }
 
     trait :rendered do
-      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context).bootstrap(:carousel, attributes) }
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:carousel, attributes) }
     end
   end
 
@@ -110,7 +132,8 @@ FactoryBot.define do
     initialize_with { new(attributes, ActionController::Base.new.view_context) }
 
     trait :rendered do
-      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context).bootstrap(:dropdown, attributes) }
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:dropdown, attributes) }
     end
   end
 
@@ -127,7 +150,203 @@ FactoryBot.define do
     initialize_with { new(attributes, ActionController::Base.new.view_context) }
 
     trait :rendered do
-      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context).bootstrap(:embed, attributes) }
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:embed, attributes) }
+    end
+  end
+
+  factory :bootstrap_form, class: NfgUi::Bootstrap::Components::Form do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:form, attributes) }
+    end
+  end
+
+  factory :bootstrap_input_group, class: NfgUi::Bootstrap::Components::InputGroup do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:input_group, attributes) }
+    end
+  end
+
+  factory :bootstrap_jumbotron, class: NfgUi::Bootstrap::Components::Jumbotron do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:jumbotron, attributes) }
+    end
+  end
+
+  factory :bootstrap_list_group, class: NfgUi::Bootstrap::Components::ListGroup do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:list_group, attributes) }
+    end
+  end
+
+  factory :bootstrap_list_group_item, class: NfgUi::Bootstrap::Components::ListGroupItem do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:list_group_item, attributes) }
+    end
+  end
+
+  factory :media_object, class: NfgUi::Bootstrap::Components::MediaObject do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:media_object, attributes) }
+    end
+  end
+
+  factory :bootstrap_modal, class: NfgUi::Bootstrap::Components::Modal do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:modal, attributes) }
+    end
+  end
+
+  factory :bootstrap_nav, class: NfgUi::Bootstrap::Components::Nav do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:nav, attributes) }
+    end
+  end
+
+  factory :bootstrap_navbar, class: NfgUi::Bootstrap::Components::Navbar do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:navbar, attributes) }
+    end
+  end
+
+  factory :bootstrap_pagination, class: NfgUi::Bootstrap::Components::Pagination do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:pagination, attributes) }
+    end
+  end
+
+  factory :bootstrap_popover, class: NfgUi::Bootstrap::Components::Popover do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:popover, attributes) }
+    end
+  end
+
+  factory :bootstrap_progress, class: NfgUi::Bootstrap::Components::Progress do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:progress, attributes) }
+    end
+  end
+
+  factory :bootstrap_table, class: NfgUi::Bootstrap::Components::Table do
+    body nil
+    data nil
+    id nil
+    traits []
+
+    skip_create
+    initialize_with { new(attributes, ActionController::Base.new.view_context) }
+
+    trait :rendered do
+      initialize_with { NfgUi::UI::Base.new(ActionController::Base.new.view_context)
+                                       .bootstrap(:table, attributes) }
     end
   end
 end
