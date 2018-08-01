fc466463efbb4e923782e5bcc3950f3cca0eb20a
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-08-01 07:38:21 -0400

    adds view spec for input group

--- spec/views/nfg_ui/bootstrap/_input_group.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/_input_group.html.haml_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe 'nfg_ui/bootstrap/_input_group.html.haml', type: :view do
   let(:input_group) { FactoryBot.create(:bootstrap_input_group, body: body) }
   subject { render 'nfg_ui/bootstrap/input_group', input_group: input_group }
 
-  pending 'coming soon'
-
   it 'outputs a bootstrap input_group component in html' do
+    expect(subject).to have_css '.input-group'
+    expect(subject).to have_selector '.input-group', text: body
+    expect(subject).to eq "<div class='input-group'>\n#{body}\n</div>\n"
   end
 end
