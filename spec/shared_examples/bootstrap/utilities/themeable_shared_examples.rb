59e583207c7da2fa0055727654cf2cb1600ce3c2
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-08-01 20:15:56 -0400

    Fixes error in themeable shared examples (copy/paste problem from dismissible shared examples)

--- spec/shared_examples/bootstrap/utilities/themeable_shared_examples.rb
+++ spec/shared_examples/bootstrap/utilities/themeable_shared_examples.rb
@@ -3,7 +3,7 @@ shared_examples_for 'a component that includes the Themeable utility module' do
     let(:component) { described_class.new({}, ActionController::Base.new.view_context) }
     it { expect(described_class.included_modules).to include NfgUi::Bootstrap::Utilities::Themeable }
     it 'responds to the dismissible public methods' do
-      expect(component).to respond_to :dismissible, :dismissible?
+      expect(component).to respond_to :theme, :outlined
     end 
   end
 end
