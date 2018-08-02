027f167dcc61ffe399a2dbf52f9e2a5e91f91825
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-08-02 13:46:21 -0400

    Adds titleable shared examples.

---
+++ spec/shared_examples/bootstrap/utilities/titleable_shared_examples.rb
@@ -0,0 +1,9 @@
+shared_examples_for 'a component that includes the Titleable utility module' do
+  describe 'Titleable module included in the component' do
+    let(:component) { described_class.new({}, ActionController::Base.new.view_context) }
+    it { expect(described_class.included_modules).to include NfgUi::Components::Utilities::Titleable }
+    it 'responds to the dismissible public methods' do
+      expect(component).to respond_to :title
+    end
+  end
+end
