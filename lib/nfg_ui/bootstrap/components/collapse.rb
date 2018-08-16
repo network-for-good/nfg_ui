d762e395b6dbfb0bd0de01f9c354bde11d6b0bab
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-08-16 16:36:14 -0400

    Adds collapse first pass for revamp.

--- lib/nfg_ui/bootstrap/components/collapse.rb
+++ lib/nfg_ui/bootstrap/components/collapse.rb
@@ -4,22 +4,63 @@ module NfgUi
       # Bootstrap Collapse Component
       # https://getbootstrap.com/docs/4.1/components/collapse/
       class Collapse < Bootstrap::Components::Base
-        include Bootstrap::Utilities::Collapsible
+        # include Bootstrap::Utilities::Collapsible
 
         # def initialize(*)
         #   super
         #   @body = collapsible_wrapper_html
         # end
 
-        # private
+        def id
+          p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
+          if options[:id].present?
+            options[:id]
+          else
+            raise ArgumentError.new "#{self.class.name} requires options[:id]. You must pass in an :id to the options hash\n\nExample:\n= ui.bootstrap :collapse, id: 'required_id', body: 'Your body'"
+          end
+        end
 
+
+        #   super
+        #   # if options[:id].present?
+        #   #   super
+        #   # else
+        #   #   raise ArgumentError.new("#{self.class.name} is missing options[:id]\n :id must be included in the component's options hash is required.")
+        #   # end
+        # end
+
+        def collapsed
+          p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
+          options.fetch(:collapsed, default_collapsed)
+        end
+
+        def html_options
+          p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
+          super.merge!(id: id)
+        end
+
+        private
+
+
+        
+        def css_classes
+          p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
+          collapsed ? super : super + ' show'
+        end
+        
         # def component_css_class
         #   nil
         # end
 
-        # def default_collapsed
-        #   true
-        # end
+        def non_html_attribute_options
+          p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
+          super.push(:collapsed)
+        end
+
+        def default_collapsed
+          p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
+          true
+        end
       end
     end
   end
--- spec/lib/nfg_ui/bootstrap/components/collapse_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/collapse_spec.rb
@@ -1,8 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe NfgUi::Bootstrap::Components::Collapse do
+  let(:collapse) { FactoryBot.create(:bootstrap_collapse, **options) }
+  let(:options) { { id: test_id } }
+  let(:test_id) { 'test_id' }
   subject { described_class }
+  
   it { is_expected.to be < NfgUi::Bootstrap::Components::Base }
-  it_behaves_like 'a component with a consistent initalized construction'
-  it_behaves_like 'a component that includes the Collapsible utility module'
+  it_behaves_like 'a component with a consistent initalized construction', required_options: { id: 'test_id' }
+  # it_behaves_like 'a component that includes the Collapsible utility module'
+
+  describe '#id' do
+    let(:view_context) { ActionController::Base.new.view_context }
+    let(:collapse) { NfgUi::Bootstrap::Components::Collapse.new(options, view_context) }
+    let(:options) { {} }
+    subject { collapse.id }
+
+    context 'when collapse id is present' do
+      let(:options) { { id: test_id } }
+      it { expect { subject }.not_to raise_error }
+      it { is_expected.to eq test_id }
+    end
+
+    context 'when collapse id is not present' do
+      let(:options) { {} }
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
+  end
 end
