bf96cb8a12c95c8fd8563c73e6fb756a48f83f86
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-09-27 09:13:14 -0400

    Updates spec

--- spec/lib/nfg_ui/bootstrap/components/card_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/card_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe NfgUi::Bootstrap::Components::Card do
+  let(:alert) { described_class.new(options, ActionController::Base.new.view_context) }
+  let(:options) { {} }
+  
   subject { described_class }
+  
   it { is_expected.to be < NfgUi::Bootstrap::Components::Base }
   it_behaves_like 'a component with a consistent initalized construction'
+  
 end
