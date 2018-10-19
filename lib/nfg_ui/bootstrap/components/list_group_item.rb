b9fb23f4a1479bdaca338d2173e6fc268f2aa910
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-19 16:00:38 -0400

    builds out list-group and list-group-item -- begins list group item view spec.

--- lib/nfg_ui/bootstrap/components/list_group_item.rb
+++ lib/nfg_ui/bootstrap/components/list_group_item.rb
@@ -6,11 +6,43 @@ module NfgUi
       # Bootstrap List Group Item Component
       # https://getbootstrap.com/docs/4.1/components/list-group/
       class ListGroupItem < Bootstrap::Components::Base
-        
+        include Bootstrap::Utilities::Wrappable
+        include Bootstrap::Utilities::Activatable
+        include Bootstrap::Utilities::Disableable
+        include Bootstrap::Utilities::Themeable
         # private
         
         def component_family
           :list_group
+        end
+
+        def action
+          options.fetch(:action, false)
+        end
+
+        def badge
+          options.fetch(:badge, nil)
+        end
+
+        private
+
+        def default_theme
+          nil
+        end
+
+        def default_html_wrapper_element
+          :li
+        end
+
+        def css_classes
+          return super unless action || badge
+          super +
+            (action ? " #{component_css_class}-action" : '') +
+            (badge ? ' d-flex justify-content-between align-items-center' : '')
+        end
+
+        def non_html_attribute_options
+          super.push(:action, :badge)
         end
       end
     end
--- spec/views/nfg_ui/bootstrap/list_groups/_list_group_item.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/list_groups/_list_group_item.html.haml_spec.rb
@@ -2,11 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'nfg_ui/bootstrap/list_groups/_list_group_item.html.haml', type: :view do
   let(:body) { 'test body content' }
-  let(:list_group_item) { FactoryBot.create(:bootstrap_list_group_item, body: body) }
-  subject { render 'nfg_ui/bootstrap/list_group_item', list_group_item: list_group_item }
+  let(:list_group_item) { FactoryBot.create(:bootstrap_list_group_item, *options, body: body) }
+  let(:options) { {} }
 
-  pending 'coming soon'
+  subject { render 'nfg_ui/bootstrap/list_groups/list_group_item', list_group_item: list_group_item }
 
-  it 'outputs a bootstrap list_group_item component in html' do
+  describe 'a standard list-group-item with no additional options' do
+    let(:options) { {} }
+  end
+
+  describe 'a disabled list-group-item' do
+    let(:options) { { disabled: true } }
+  end
+
+  describe 'a thematic list-group-item' do
+    let(:options) { { disabled: true } }
+  end
+
+  describe 'an action list-group-item' do
+    let(:options) { { disabled: true } }
+  end
+
+  describe 'an active list-group-item' do
+    let(:options) { { disabled: true } }
+  end
+
+  describe 'a custom element (:as) list-group-item' do
+    let(:options) { { disabled: true } }
   end
 end
