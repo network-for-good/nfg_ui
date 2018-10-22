90aa3d7080415a9aaab11fbee073b5b33db270fb
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-22 16:47:48 -0400

    Updates incorrect name of component.

--- spec/lib/nfg_ui/bootstrap/components/nav_item_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/nav_item_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe NfgUi::Bootstrap::Components::NavItem do
-  let(:nav) { described_class.new(options, ActionController::Base.new.view_context) }
+  let(:nav_item) { described_class.new(options, ActionController::Base.new.view_context) }
   let(:options) { {} }
 
   it { expect(described_class).to be < NfgUi::Bootstrap::Components::Base }
@@ -9,7 +9,7 @@ RSpec.describe NfgUi::Bootstrap::Components::NavItem do
   it_behaves_like 'a component that includes the Wrappable utility module'
 
   describe '#active' do
-    subject { nav.active }
+    subject { nav_item.active }
     context 'when active is true in options' do
       let(:options) { { active: true } }
       it { is_expected.to be }
@@ -27,7 +27,7 @@ RSpec.describe NfgUi::Bootstrap::Components::NavItem do
   end
 
   describe '#button' do
-    subject { nav.button }
+    subject { nav_item.button }
     context 'when button is present in options' do
       let(:tested_button) { 'test button' }
       let(:options) { { button: tested_button } }
@@ -46,12 +46,12 @@ RSpec.describe NfgUi::Bootstrap::Components::NavItem do
   end
 
   describe '#component_family' do
-    subject { nav.component_family }
+    subject { nav_item.component_family }
     it { is_expected.to eq :nav }
   end
 
   describe '#disabled' do
-    subject { nav.disabled }
+    subject { nav_item.disabled }
     context 'when disabled is true in options' do
       let(:options) { { disabled: true } }
       it { is_expected.to be }
@@ -69,7 +69,7 @@ RSpec.describe NfgUi::Bootstrap::Components::NavItem do
   end
 
   describe '#dropdown' do
-    subject { nav.dropdown }
+    subject { nav_item.dropdown }
     context 'when dropdown is true in options' do
       let(:options) { { dropdown: true } }
       it { is_expected.to be }
@@ -87,7 +87,7 @@ RSpec.describe NfgUi::Bootstrap::Components::NavItem do
   end
 
   describe '#href' do
-    subject { nav.href }
+    subject { nav_item.href }
     context 'when href is present in options' do
       let(:tested_href) { 'tested-href' }
       let(:options) { { href: tested_href } }
@@ -101,7 +101,7 @@ RSpec.describe NfgUi::Bootstrap::Components::NavItem do
   end
 
   describe '#include_nav_link?' do
-    subject { nav.include_nav_link? }
+    subject { nav_item.include_nav_link? }
     context 'when href is present' do
       let(:options) { { href: tested_href, as: tested_as, dropdown: tested_dropdown } }
       let(:tested_href) { nil }
@@ -145,16 +145,35 @@ RSpec.describe NfgUi::Bootstrap::Components::NavItem do
           it { is_expected.to be }
         end
       end
+    end
 
-      context 'when dropdown is false' do
-        let(:tested_dropdown) { false }
-        it { is_expected.not_to be }
-      end
+    context 'when tab is true' do
+      let(:options) { { tab: true } }
+      it { is_expected.to be }
+    end
+  end
+
+  describe '#tab' do
+    subject { nav_item.tab }
+    context 'when tab is true in options' do
+      let(:tested_tab) { 'tested-tab' }
+      let(:options) { { tab: tested_tab } }
+      it { is_expected.to eq tested_tab }
+    end
+
+    context 'when tab is nil in options' do
+      let(:options) { { tab: nil } }
+      it { is_expected.to be_nil }
+    end
+
+    context 'when tab is not present in options' do
+      let(:options) { {} }
+      it { is_expected.to be_nil }
     end
   end
 
   describe '#automatic_as' do
-    subject { nav.send(:automatic_as) }
+    subject { nav_item.send(:automatic_as) }
 
     context 'when include_nav_link? is true' do
       let(:options) { { dropdown: true } }
@@ -163,25 +182,25 @@ RSpec.describe NfgUi::Bootstrap::Components::NavItem do
 
     context 'when include_nav_link? is false' do
       let(:options) { { dropdown: false } }
-      it { is_expected.to eq nav.send(:default_html_wrapper_element) }
+      it { is_expected.to eq nav_item.send(:default_html_wrapper_element) }
 
       context 'and when :as is supplied in options' do
         let(:options) { { as: :strong } }
 
         it 'still utilizes the default_html_wrapper_element' do
-          expect(subject).to eq nav.send(:default_html_wrapper_element)
+          expect(subject).to eq nav_item.send(:default_html_wrapper_element)
         end
       end
 
       context 'and when :as is not supplied in options' do
         let(:options) { {} }
-        it { is_expected.to eq nav.send(:default_html_wrapper_element) }
+        it { is_expected.to eq nav_item.send(:default_html_wrapper_element) }
       end
     end
   end
 
   describe '#css_classes' do
-    subject { nav.send(:css_classes) }
+    subject { nav_item.send(:css_classes) }
 
     context 'when :dropdown is true' do
       let(:options) { { dropdown: true } }
@@ -195,7 +214,17 @@ RSpec.describe NfgUi::Bootstrap::Components::NavItem do
   end
 
   describe '#default_html_wrapper_element' do
-    subject { nav.send(:default_html_wrapper_element) }
+    subject { nav_item.send(:default_html_wrapper_element) }
     it { is_expected.to eq :li }
+  end
+
+  describe '#non_html_attribute_options' do
+    subject { nav_item.send(:non_html_attribute_options) }
+    it { is_expected.to include(:active,
+                                :disabled,
+                                :dropdown,
+                                :button,
+                                :href,
+                                :tab) }
   end
 end
