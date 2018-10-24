9e5023805c3c5d81220cfcbb5f88de92d44f3653
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-24 09:05:11 -0400

    Collapse audit complete.

--- lib/nfg_ui/bootstrap/components/collapse.rb
+++ lib/nfg_ui/bootstrap/components/collapse.rb
@@ -4,63 +4,26 @@ module NfgUi
       # Bootstrap Collapse Component
       # https://getbootstrap.com/docs/4.1/components/collapse/
       class Collapse < Bootstrap::Components::Base
-        # include Bootstrap::Utilities::Collapsible
-
-        # def initialize(*)
-        #   super
-        #   @body = collapsible_wrapper_html
-        # end
-
-        # def id
-        #   p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
-        #   if options[:id].present?
-        #     options[:id]
-        #   else
-        #     raise ArgumentError.new "#{self.class.name} requires options[:id]. You must pass in an :id to the options hash\n\nExample:\n= ui.bootstrap :collapse, id: 'required_id', body: 'Your body'"
-        #   end
-        # end
-        
-        #   super
-        #   # if options[:id].present?
-        #   #   super
-        #   # else
-        #   #   raise ArgumentError.new("#{self.class.name} is missing options[:id]\n :id must be included in the component's options hash is required.")
-        #   # end
-        # end
+        def collapsed
+          options.fetch(:collapsed, true)
+        end
 
         def navbar
           options.fetch(:navbar, false)
         end
 
-        def collapsed
-          p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
-          options.fetch(:collapsed, default_collapsed)
-        end
-
         private
-        
+
         def css_classes
-          p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
           [
             super,
             ('show' unless collapsed),
             ('navbar-collapse' if navbar)
           ].join(' ').squish
-          # collapsed ? super : super + ' show'
         end
-        
-        # def component_css_class
-        #   nil
-        # end
 
         def non_html_attribute_options
-          p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
           super.push(:collapsed, :navbar)
-        end
-
-        def default_collapsed
-          p "====== Printed from: (Bootstrap::Components::Collapse) self.class.name: #{self.class.name} method: #{__method__}"
-          true
         end
       end
     end
--- spec/lib/nfg_ui/bootstrap/components/collapse_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/collapse_spec.rb
@@ -4,32 +4,13 @@ RSpec.describe NfgUi::Bootstrap::Components::Collapse do
   let(:collapse) { described_class.new(options, ActionController::Base.new.view_context) }
   let(:options) { { id: test_id } }
   let(:test_id) { 'test_id' }
-  
+
   it { expect(described_class).to be < NfgUi::Bootstrap::Components::Base }
   it_behaves_like 'a component with a consistent initalized construction', required_options: { id: 'test_id' }
-  # it_behaves_like 'a component that includes the Collapsible utility module'
-
-  # describe '#id' do
-  #   let(:view_context) { ActionController::Base.new.view_context }
-  #   let(:collapse) { NfgUi::Bootstrap::Components::Collapse.new(options, view_context) }
-  #   let(:options) { {} }
-  #   subject { collapse.id }
-
-  #   context 'when collapse id is present' do
-  #     let(:options) { { id: test_id } }
-  #     it { expect { subject }.not_to raise_error }
-  #     it { is_expected.to eq test_id }
-  #   end
-
-  #   context 'when collapse id is not present' do
-  #     let(:options) { {} }
-  #     it { expect { subject }.to raise_error(ArgumentError) }
-  #   end
-  # end
 
   describe '#collapsed' do
     subject { collapse.collapsed }
-    
+
     context 'when :collapsed is present within options' do
       let(:test_collapsed) { false }
       let(:options) { { id: test_id, collapsed: test_collapsed } }
@@ -44,7 +25,7 @@ RSpec.describe NfgUi::Bootstrap::Components::Collapse do
 
   describe '#navbar' do
     subject { collapse.navbar }
-    
+
     context 'when :navbar is present within options' do
       let(:options) { { id: test_id, navbar: true } }
       it { is_expected.to be }
@@ -59,7 +40,7 @@ RSpec.describe NfgUi::Bootstrap::Components::Collapse do
   describe '#css_classes' do
     subject { collapse.send(:css_classes) }
     let(:options) { { id: test_id, collapsed: test_collapsed } }
-    
+
     context 'when the component has :collapsed set as true' do
       let(:test_collapsed) { true }
       it { is_expected.not_to include 'show' }
@@ -84,10 +65,5 @@ RSpec.describe NfgUi::Bootstrap::Components::Collapse do
   describe '#non_html_attribute_options' do
     subject { collapse.send(:non_html_attribute_options) }
     it { is_expected.to include :collapsed, :navbar }
-  end
-
-  describe '#default_collapsed' do
-    subject { collapse.send(:default_collapsed) }
-    it { is_expected.to be }
   end
 end
