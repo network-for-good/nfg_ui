690d90aa8b8e1f6aeeeb10344adee5fb2da1ce10
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-24 08:55:22 -0400

    button group audit complete.

--- lib/nfg_ui/bootstrap/components/button_group.rb
+++ lib/nfg_ui/bootstrap/components/button_group.rb
@@ -8,15 +8,6 @@ module NfgUi
       # https://getbootstrap.com/docs/4.1/components/breadcrumb/
       class ButtonGroup < Bootstrap::Components::Base
         include Bootstrap::Utilities::Sizable
-        # include Bootstrap::Utilities::AriaAssistable
-
-        # attr_reader :toolbar
-
-        # def initialize(*)
-        #   super
-        #   build_aria(aria_key: :label, aria_value: 'action buttons')
-        #   @toolbar = traits.include?(:toolbar)
-        # end
 
         def vertical
           options.fetch(:vertical, false)
@@ -24,18 +15,14 @@ module NfgUi
 
         private
 
-        def component_css_class
-          p "====== Printed from: (Bootstrap::Components::ButtonGroup) self.class.name: #{self.class.name} method: #{__method__}"
-          'btn-group'
-        end
-
-        # def trait_css_classes
-        #   traits.include?(:vertical) ? "#{component_css_class}-vertical" : super
-        # end
-
         def assistive_html_attributes
           p "====== Printed from: (Bootstrap::Components::ButtonGroup) self.class.name: #{self.class.name} method: #{__method__}"
           super.merge!(role: 'group', aria: { label: 'action buttons' })
+        end
+
+        def component_css_class
+          p "====== Printed from: (Bootstrap::Components::ButtonGroup) self.class.name: #{self.class.name} method: #{__method__}"
+          'btn-group'
         end
       end
     end
--- spec/lib/nfg_ui/bootstrap/components/button_group_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/button_group_spec.rb
@@ -10,16 +10,6 @@ RSpec.describe NfgUi::Bootstrap::Components::ButtonGroup do
   it_behaves_like 'a component with a consistent initalized construction'
   it_behaves_like 'a component that includes the Sizable utility module'
 
-  describe '#component_css_class' do
-    subject { button_group.send(:component_css_class) }
-    it { is_expected.to eq 'btn-group' }
-  end
-
-  describe '#assistive_html_attributes' do
-    subject { button_group.send(:assistive_html_attributes) }
-    it { is_expected.to eq(role: 'group', aria: { label: 'action buttons' }) }
-  end
-
   describe '#vertical' do
     let(:options) { { vertical: tested_vertical } }
     let(:tested_vertical) { nil }
@@ -44,5 +34,15 @@ RSpec.describe NfgUi::Bootstrap::Components::ButtonGroup do
       let(:options) { {} }
       it { is_expected.to be_falsey }
     end
+  end
+
+  describe '#assistive_html_attributes' do
+    subject { button_group.send(:assistive_html_attributes) }
+    it { is_expected.to eq(role: 'group', aria: { label: 'action buttons' }) }
+  end
+
+  describe '#component_css_class' do
+    subject { button_group.send(:component_css_class) }
+    it { is_expected.to eq 'btn-group' }
   end
 end
