9d3ffb7609efc747fcad21a9e4e1a02264444748
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-23 22:16:55 -0400

    cleans up button spec. adds spec for href.

--- lib/nfg_ui/bootstrap/components/button.rb
+++ lib/nfg_ui/bootstrap/components/button.rb
@@ -14,39 +14,13 @@ module NfgUi
         include Bootstrap::Utilities::Tooltipable
         include Bootstrap::Utilities::Collapsible
 
-        # attr_reader :block, :modal
-
-        # def initialize(*)
-        #   super
-        #   @modal = options.fetch(:modal, nil)
-        #   @block = options.fetch(:block, default_block)
-        #   data.merge!(toggle: 'modal', target: "##{modal}") if modal
-        #   build_aria(aria_key: :pressed, aria_value: true) if active?
-        # end
-
-        # def block?
-        #   block
-        # end
-
         def block
-          p "====== Printed from: (Bootstrap::Components::Button) self.class.name: #{self.class.name} method: #{__method__}"
           options.fetch(:block, false)
         end
 
-        def modal
-          p "====== Printed from: (Bootstrap::Components::Button) self.class.name: #{self.class.name} method: #{__method__}"
-          options.fetch(:modal, nil)
+        def data
+          modal ? super.merge!(toggle: 'modal', target: options[:modal]) : super
         end
-
-        # def html_options
-        #   p "====== Printed from: (Bootstrap::Components::Button) self.class.name: #{self.class.name} method: #{__method__}"
-        #   return super unless as == :a && (options[:collapse].present? || options[:modal].present?)
-        #   if options[:collapse].present?
-        #     super.merge!(href: collapse)
-        #   elsif options[:modal].present?
-        #     options[:href].nil? ? super.merge!(href: '#') : super
-        #   end
-        # end
 
         def href
           return if options[:as] == :button
@@ -59,10 +33,8 @@ module NfgUi
           end
         end
 
-        def data
-          p "====== Printed from: (Bootstrap::Components::Button) self.class.name: #{self.class.name} method: #{__method__}"
-          # return super unless options[:modal].present?
-          modal ? super.merge!(toggle: 'modal', target: options[:modal]) : super
+        def modal
+          options.fetch(:modal, nil)
         end
 
         def remove_component_css_classes
@@ -71,34 +43,28 @@ module NfgUi
 
         private
 
+        def assistive_html_attributes
+          active ? super.merge(aria: { pressed: true }) : super
+        end
+
+        def css_classes
+          block ? super + " #{component_css_class}-block" : super
+        end
+
         def collapse_data_attributes
-          p "====== Printed from: (Bootstrap::Utilities::Collapsible) self.class.name: #{self.class.name} method: #{__method__}"
           as == :a ? super.except!(:target) : super
         end
 
         def component_css_class
-          p "====== Printed from: (Bootstrap::Components::Button) self.class.name: #{self.class.name} method: #{__method__}"
           remove_component_css_classes ? '' : 'btn'
         end
 
-        def css_classes
-          p "====== Printed from: (Bootstrap::Components::Button) self.class.name: #{self.class.name} method: #{__method__}"
-          block ? super + " #{component_css_class}-block" : super
-        end
-
         def default_html_wrapper_element
-          p "====== Printed from: (Bootstrap::Components::Button) self.class.name: #{self.class.name} method: #{__method__}"
           :a
         end
 
         def non_html_attribute_options
-          p "====== Printed from: (Bootstrap::Components::Button) self.class.name: #{self.class.name} method: #{__method__}"
           super.push(:modal, :block, :remove_component_css_classes)
-        end
-
-        def assistive_html_attributes
-          p "====== Printed from: (Bootstrap::Components::Button) self.class.name: #{self.class.name} method: #{__method__}"
-          active ? super.merge(aria: { pressed: true }) : super
         end
       end
     end
--- spec/lib/nfg_ui/bootstrap/components/button_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/button_spec.rb
@@ -40,6 +40,79 @@ RSpec.describe NfgUi::Bootstrap::Components::Button do
     end
   end
 
+  describe '#data' do
+    subject { button.data }
+
+    describe 'modal implications on the component data attribute' do
+      context 'when :modal is present in options' do
+        let(:data_hash) { {} }
+        let(:modal) { '#test_modal' }
+        let(:options) { { data: data_hash, modal: modal } }
+
+        context 'when data is already present' do
+          let(:data_hash) { { test: 'value' } }
+          it { is_expected.to eq(**data_hash, toggle: 'modal', target: "#{modal}") }
+        end
+
+        context 'when data is not already present' do
+          it { is_expected.to eq({ toggle: 'modal', target: "#{modal}" }) }
+        end
+      end
+
+      context 'when :modal is not present in options' do
+        let(:options) { {} }
+        it { is_expected.to eq({}) }
+      end
+    end
+  end
+
+  describe '#href' do
+    subject { button.href }
+    context 'when :as is :button' do
+      let(:options) { { as: :button } }
+      it { is_expected.to be_nil }
+    end
+
+    context 'when :as is not button' do
+      let(:options) { {} }
+      let(:tested_collapse) { '#tested_collapse' }
+      let(:tested_modal) { '#tested_modal' }
+      
+      context 'href with collapse' do
+        let(:options) { { collapse: tested_collapse } }
+        context 'when collapse is truthy' do
+          it { is_expected.to eq tested_collapse }
+        end
+
+        context 'when collapse is falsey' do
+          let(:options) { { collapse: false } }
+          it { is_expected.to be_nil }
+        end
+      end
+
+      context 'href with modal' do
+        context 'when modal is truthy' do
+          let(:options) { { modal: tested_modal, href: tested_href } }
+          let(:tested_href) { nil }
+          context 'when href is present' do
+            let(:tested_href) { '#tested_href' }
+            it { is_expected.to eq tested_href }
+          end
+
+          context 'when href is not present' do
+            let(:tested_href) { nil }
+            it { is_expected.to eq '#' }
+          end
+        end
+
+        context 'when modal is falsey' do
+          let(:options) { { modal: false } }
+          it { is_expected.to be_nil }
+        end
+      end
+    end
+  end
+
   describe '#remove_component_css_classes' do
     subject { button.remove_component_css_classes }
     let(:options) { { remove_component_css_classes: tested_remove_component_css_classes } }
@@ -153,31 +226,7 @@ RSpec.describe NfgUi::Bootstrap::Components::Button do
     end
   end
 
-  describe '#data' do
-    subject { button.data }
-
-    describe 'modal implications on the component data attribute' do
-      context 'when :modal is present in options' do
-        let(:data_hash) { {} }
-        let(:modal) { '#test_modal' }
-        let(:options) { { data: data_hash, modal: modal } }
-
-        context 'when data is already present' do
-          let(:data_hash) { { test: 'value' } }
-          it { is_expected.to eq(**data_hash, toggle: 'modal', target: "#{modal}") }
-        end
-
-        context 'when data is not already present' do
-          it { is_expected.to eq({ toggle: 'modal', target: "#{modal}" }) }
-        end
-      end
-
-      context 'when :modal is not present in options' do
-        let(:options) { {} }
-        it { is_expected.to eq({}) }
-      end
-    end
-  end
+  
 
   describe '#component_css_class' do
     subject { button.send(:component_css_class) }
