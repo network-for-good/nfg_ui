b0b3c260d5cb19e14408568f821a5ca0a542e2f9
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-24 09:05:05 -0400

    carousel audit complete.

--- spec/lib/nfg_ui/bootstrap/components/carousel_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/carousel_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe NfgUi::Bootstrap::Components::Carousel do
   let(:carousel) { described_class.new(options, ActionController::Base.new.view_context) }
-  let(:options) { {} } 
+  let(:options) { {} }
   it { expect(described_class).to be < NfgUi::Bootstrap::Components::Base }
   it_behaves_like 'a component with a consistent initalized construction'
 end
