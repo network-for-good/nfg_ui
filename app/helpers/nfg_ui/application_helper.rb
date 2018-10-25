a063aa70e8d5725b84a3739e25785b170faa5201
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-25 16:04:49 -0400

    memoizes the ui method.

--- app/helpers/nfg_ui/application_helper.rb
+++ app/helpers/nfg_ui/application_helper.rb
@@ -7,8 +7,7 @@ module NfgUi
     # Network for Good components: = ui.nfg
     # Bootstrap core components:   = ui.bootstrap
     def ui
-      p "---------- NfgUi:ApplicationHelper.ui - STARTS ----------"
-      NfgUi::UI::Base.new(controller.view_context)
+      @ui ||= NfgUi::UI::Base.new(controller.view_context)
     end
   end
 end
