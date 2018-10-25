c8a44bacc14fc9f6f883d3e73ad20920dea87d7c
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-24 21:43:16 -0400

    wraps up main pagination component.

--- spec/lib/nfg_ui/bootstrap/components/pagination_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/pagination_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe NfgUi::Bootstrap::Components::Pagination do
-  subject { described_class }
-  it { is_expected.to be < NfgUi::Bootstrap::Components::Base }
+  let(:pagination) { described_class.new(options, ActionController::Base.new.view_context) }
+  let(:options) { {} }
+
+  it { expect(described_class).to be < NfgUi::Bootstrap::Components::Base }
   it_behaves_like 'a component with a consistent initalized construction'
+  it_behaves_like 'a component that includes the Alignable utility module'
+  it_behaves_like 'a component that includes the Sizable utility module'
+
+  describe '#component_family' do
+    subject { pagination.component_family }
+    it { is_expected.to eq :pagination }
+  end
+
+  describe 'private methods' do
+    describe '#css_classes' do
+      subject { nav.send(:css_classes) }
+    end
+  end
 end
--- spec/views/nfg_ui/bootstrap/paginations/_pagination.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/paginations/_pagination.html.haml_spec.rb
@@ -1,12 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe 'nfg_ui/bootstrap/paginations/_pagination.html.haml', type: :view do
-  let(:body) { 'test body content' }
-  let(:pagination) { FactoryBot.create(:bootstrap_pagination, body: body) }
+  let(:pagination) { FactoryBot.create(:bootstrap_pagination, **options) }
+  let(:options) { {} }
   subject { render 'nfg_ui/bootstrap/paginations/pagination', pagination: pagination }
 
-  it 'outputs a bootstrap pagination component in html' do
-    expect(subject).to have_selector '.pagination', text: body
-    expect(subject).to eq "<div class='pagination'>\n#{body}\n</div>\n"
+  describe 'component html' do
+    let(:tested_body) { 'tested body' }
+    let(:options) { { body: tested_body } }
+    it 'outputs a bootstrap pagination component in html' do
+      expect(subject).to have_selector 'nav ul.pagination'
+      expect(subject).to eq "<nav>\n<ul class='pagination'>\n#{tested_body}\n</ul>\n</nav>\n"
+    end
+  end
+
+  describe 'an aligned pagination' do
+    describe 'right aligned pagination' do
+      context 'when right is true in options' do
+        let(:options) { { right: true } }
+        it 'outputs a right aligned navigation' do
+          expect(subject).to have_css 'ul.pagination.justify-content-end'
+          expect(subject).to eq "<nav>\n<ul class='pagination justify-content-end'>\n\n</ul>\n</nav>\n"
+        end
+      end
+
+      context 'when right is not present in options' do
+        let(:options) { {} }
+        it 'does not output a right aligned navigation' do
+          expect(subject).not_to have_css 'ul.pagination.justify-content-end'
+          expect(subject).to have_css 'ul.pagination'
+        end
+      end
+    end
+
+    describe 'center aligned pagination' do
+      context 'when center is true in options' do
+        let(:options) { { center: true } }
+        it 'outputs a center aligned navigation' do
+          expect(subject).to have_css 'ul.pagination.justify-content-center'
+          expect(subject).to eq "<nav>\n<ul class='pagination justify-content-center'>\n\n</ul>\n</nav>\n"
+        end
+      end
+
+      context 'when center is not present in options' do
+        let(:options) { {} }
+        it 'does not output a center aligned navigation' do
+          expect(subject).not_to have_css 'ul.pagination.justify-content-center'
+          expect(subject).to have_css 'ul.pagination'
+        end
+      end
+    end
+  end
+
+  describe 'resized pagination' do
+    context 'when size is present' do
+      let(:tested_size) { :lg }
+      let(:options) { { size: tested_size } }
+      it 'outputs a resized pagination component' do
+        expect(subject).to have_css "ul.pagination.pagination-#{tested_size}"
+        expect(subject).to eq "<nav>\n<ul class='pagination pagination-lg'>\n\n</ul>\n</nav>\n"
+      end
+    end
+
+    context 'when size is not present' do
+      let(:options) { {} }
+      it 'does not output a resized pagination component' do
+        expect(subject).not_to have_css "[class*='pagination-']"
+      end
+    end
   end
 end
