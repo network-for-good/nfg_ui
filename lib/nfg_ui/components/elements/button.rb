4050afab35f4742740a2ab9117ea16c59a7a8176
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-27 21:28:13 -0400

    Makes design sys button traitable.

--- lib/nfg_ui/components/elements/button.rb
+++ lib/nfg_ui/components/elements/button.rb
@@ -7,8 +7,9 @@ module NfgUi
       # As such, the NFG UI Button is simply a bootstrap Button behind the scenes.
       # Traits will eventually be connected here.
       class Button < Bootstrap::Components::Button
+        include NfgUi::Components::Utilities::Traitable
         # include NfgUi::Components::Traits::Button
-        # include NfgUi::Components::Utilities::Iconable
+        include NfgUi::Components::Utilities::Iconable
         # include Bootstrap::Utilities::Tooltipable
 
         # attr_reader :disable_with, :data, :remote
