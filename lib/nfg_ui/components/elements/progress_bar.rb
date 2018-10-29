d934d04976d83c5859d2f2e90191a92d1ad02e53
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-29 01:33:19 -0400

    Sets progress bar to tooltipable.

--- lib/nfg_ui/components/elements/progress_bar.rb
+++ lib/nfg_ui/components/elements/progress_bar.rb
@@ -8,6 +8,20 @@ module NfgUi
         include NfgUi::Components::Utilities::Traitable
         include NfgUi::Components::Utilities::Describable
         include Bootstrap::Utilities::Tooltipable
+        include NfgUi::Components::Traits::Theme
+
+        # need to pass the tooltip to the wrapper
+        def data
+          tooltip ? super.except(:toggle, :placement, :html) : super
+        end
+
+        def html_options
+          tooltip ? super.except(:title) : super
+        end
+
+        def progress_wrapper_html_options
+          tooltip ? super.merge!(data: tooltip_data_attributes, title: tooltip) : super
+        end
       end
     end
   end
