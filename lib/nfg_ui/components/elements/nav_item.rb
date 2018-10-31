22fb08549dcf59612b44ba305e4b6fc1f2df297f
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-10-31 11:03:59 -0400

    adds tooltips to nav item

--- lib/nfg_ui/components/elements/nav_item.rb
+++ lib/nfg_ui/components/elements/nav_item.rb
@@ -5,6 +5,8 @@ module NfgUi
     module Elements
       # Nav doc coming soon
       class NavItem < Bootstrap::Components::NavItem
+        include Bootstrap::Utilities::Tooltipable
+
         include NfgUi::Components::Utilities::Traitable
         include NfgUi::Components::Utilities::Describable
         include NfgUi::Components::Utilities::Renderable
