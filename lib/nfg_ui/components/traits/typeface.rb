ac4f7a805912bc732a34d8123edbe527bae475c1
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-11-01 14:12:50 -0400

    adds note about spec to write

--- lib/nfg_ui/components/traits/typeface.rb
+++ lib/nfg_ui/components/traits/typeface.rb
@@ -15,6 +15,12 @@ module NfgUi
           options[:as] = body_tag
         end
 
+        # TODO: this was a tricky one and needs specs (as well as other traits need this spec)
+        # Write a spec that tests that the typeface is ACTUALLY a caption
+        # this was identified earlier as not happening because we'd set the as option
+        # but it wasn't spitting out the caption css_class...
+        # after some exploring it turned out the component didn't understand that it
+        # was actually a caption... it just looked like one.
         def caption_trait
           options[:as] = caption_tag
           options[:caption] = body
