ece78bac526156781a3e372893f61289297931ac
Timothy King <tmk@lordzork.com>
2018-11-01 14:56:49 -0400

    Adds back load_defaults in test app application.rb

--- spec/test_app/config/application.rb
+++ spec/test_app/config/application.rb
@@ -10,6 +10,8 @@ module TestApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     # Settings in config/environments/* take precedence over those specified here.
+    config.load_defaults 5.2
+
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
