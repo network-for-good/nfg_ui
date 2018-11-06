d644856ba464db70d97880b04ac8f5d14089f9d3
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-11-06 14:50:36 -0500

    Updates dropdown toggle to allow for a custom dropdown icon (on the right) as well as adding a custom left_icon when needed (e.g.: the sort dropdown button)

--- lib/nfg_ui/components/elements/dropdown_toggle.rb
+++ lib/nfg_ui/components/elements/dropdown_toggle.rb
@@ -7,6 +7,7 @@ module NfgUi
       # As such, the NFG UI badge is simply a bootstrap badge behind the scenes.
       # Traits will eventually be connected here.
       class DropdownToggle < Bootstrap::Components::DropdownToggle
+        include NfgUi::Components::Utilities::Iconable
         include NfgUi::Components::Utilities::Traitable
         include NfgUi::Components::Utilities::Describable
         include NfgUi::Components::Utilities::Renderable
@@ -14,6 +15,14 @@ module NfgUi
         include NfgUi::Components::Traits::DropdownToggle
         include NfgUi::Components::Traits::Size
         include NfgUi::Components::Traits::Theme
+
+        def icon
+          options.fetch(:icon, 'caret-down')
+        end
+
+        def left_icon
+          options.fetch(:left_icon, nil)
+        end
       end
     end
   end
