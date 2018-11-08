aa7f2de0fc4fa4cf62843af7c058b0822d3275a7
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-11-07 22:30:22 -0500

    adds .by support for specs. Wraps up sanity check traitable spec.

--- spec/lib/nfg_ui/components/utilities/traitable_spec.rb
+++ spec/lib/nfg_ui/components/utilities/traitable_spec.rb
@@ -14,19 +14,39 @@ RSpec.describe NfgUi::Components::Utilities::Traitable do
       let(:traits) { [:test] }
       it { is_expected.to eq traits }
     end
+
+    context 'when traits are not present' do
+      it { is_expected.to eq([]) }
+    end
   end
 
   describe 'private methods' do
+    # Sanity check
     describe '#initialize_traits' do
-
+      subject { traitable_component }
       let(:traitable_component) { view_context.ui.nfg(:alert, *traits) }
-      let(:traits) { [:secondary] }
-      
+      subject { traitable_component }
+
       context 'when traits are present' do
+        let(:traits) { [:warning] }
+
+        it 'outputs an alert with a warning theme trait applied' do
+          by 'not rendering the alert with the default theme of primary' do
+            expect(subject).not_to include "<div class='alert alert-primary"
+          end
+
+          and_by 'applying the warning theme trait to the alert' do
+            expect(subject).to include "<div class='alert alert-warning"
+          end
+        end
       end
 
       context 'when traits are not present' do
+        let(:traits) { [] }
+        it 'outputs an alert with a the default theme unmodified by a trait' do
+          expect(subject).to include "<div class='alert alert-primary"
+        end
       end
     end
-  end 
+  end
 end
