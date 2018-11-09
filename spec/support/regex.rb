62b4a7c02881321d34427a3f52ca98aa7dd2f2ff
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-11-09 14:58:17 -0500

    updates a spec that was failing silently. Updates regex helper to allow an array to be passed in for desired sub strings. Will run an .all? against the array.

--- spec/support/regex.rb
+++ spec/support/regex.rb
@@ -2,6 +2,10 @@
 
 def substring_present?(string:, starting_substring:, ending_substring:, sought_substring:)
   regex = /(?<=#{starting_substring})(.*?\n?)(?=#{ending_substring})/
-  
-  string[regex, 1].match(sought_substring)
+
+  if sought_substring.is_a?(Array)
+    sought_substring.all? { |substring| string[regex, 1].match(substring) }
+  else
+    string[regex, 1].match(sought_substring)
+  end
 end
