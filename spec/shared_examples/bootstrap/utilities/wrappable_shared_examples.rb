ab961dee37512ba6aa93509e53b899efc8ef5a0d
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-11-11 07:44:58 -0500

    wraps up shared examples for wrappable utility module; bullet proofs rendered component testing for wrappables.

--- spec/shared_examples/bootstrap/utilities/wrappable_shared_examples.rb
+++ spec/shared_examples/bootstrap/utilities/wrappable_shared_examples.rb
@@ -14,7 +14,7 @@ shared_examples_for 'a component that includes the Wrappable utility module' do 
     let(:rendered_component) { view_context.ui.send(component_suite, component_symbolic_name, ruby_component.options) }
     let(:options) { {} }
 
-    subject { rendered_component }
+    subject { rendered_component.tr('\"', "'").tr("\n", '') }
 
     context 'when :as is present in options' do
       let(:tested_as) { :span }
@@ -29,7 +29,7 @@ shared_examples_for 'a component that includes the Wrappable utility module' do 
             end
 
             and_by "adding type='button' to the assistive html attributes" do
-              expect(substring_present?(string: subject.tr('\"', "'").tr("\n", ''),
+              expect(substring_present?(string: subject,
                                         starting_substring: '<button',
                                         ending_substring: '</button>',
                                         sought_substring: "type='button'")).to be
@@ -59,7 +59,7 @@ shared_examples_for 'a component that includes the Wrappable utility module' do 
             # Note: some components overwrite href such as the dropdown toggle
             # which manually sets href to '#'
             # So, in this spec, we don't test if the href's URL is present, just that the attribute is
-            it { expect(subject.tr('\"', "'").tr("\n", '')).to include '<a ', '</a>', "href=" }
+            it { expect(subject).to include '<a ', '</a>', "href=" }
           end
 
           # Note: some components have their default html wrapper element 
@@ -87,19 +87,68 @@ shared_examples_for 'a component that includes the Wrappable utility module' do 
 
         describe 'the component with a type of submit' do
           context 'when options type is submit' do
+            let(:options) { { type: 'submit' } }
+            it 'renders the component as a button' do
+              by 'converting the component to a button' do
+                expect(subject).to include '<button', '</button>'
+              end
+
+              and_by "keeping type='submit' in the assistive html attributes" do
+                expect(substring_present?(string: subject,
+                                          starting_substring: '<button',
+                                          ending_substring: '</button>',
+                                          sought_substring: "type='submit'")).to be
+              end
+
+              and_by "not overwriting the 'submit' type for the 'button' within the assistive html attributes" do
+                expect(substring_present?(string: subject,
+                                          starting_substring: '<button',
+                                          ending_substring: '</button>',
+                                          sought_substring: "type='button'")).not_to be
+              end
+            end
           end
 
           context 'when options type is not submit' do
+            let(:tested_type) { 'not_submit_not_button' }
+            let(:options) { { type: tested_type } }
+
+            it 'does not overwrite _type_ attribute' do
+              expect(ruby_component.options[:type]).to eq tested_type
+              expect(subject).to include "type='#{tested_type}'"
+            end
+
+            context 'when the component default html element is not a button' do
+              it 'does not automatically render a button' do
+                unless ruby_component.send(:default_html_wrapper_element) == :button
+                  expect(subject).not_to include '<button', '</button>'
+                end
+              end
+            end
+
+            context 'when the component default html element is a button' do
+              it 'renders a button' do
+                if ruby_component.send(:default_html_wrapper_element) == :button
+                  expect(subject).to include '<button', '</button>'
+                  expect(subject).to include "type='#{tested_type}'"
+                end
+              end
+            end
           end
         end
       end
 
-      describe 'when neither href nor type is present in options' do
+      describe 'component without href or type is options' do
         let(:options) { {} }
-        let(:default_module_wrapper_element) { let(:test_class) { Class.new { include NfgUi::Bootstrap::Utilities::Wrappable }.new.send(:default_html_wrapper_element) } }
+        it 'renders the component with its default wrapper element' do
+          expect(subject).to include "<#{ruby_component.send(:default_html_wrapper_element)}", "</#{ruby_component.send(:default_html_wrapper_element)}>"
+        end
       end
     end
     context 'when :as is not present in options' do
+      it 'renders the component with its default wrapper element' do
+        expect(subject).to include "<#{ruby_component.send(:default_html_wrapper_element)}", "</#{ruby_component.send(:default_html_wrapper_element)}>"
+      end
     end
   end
 end
