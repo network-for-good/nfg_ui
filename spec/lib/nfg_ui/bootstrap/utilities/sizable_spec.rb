7ee6e9a971559e12236b0b107e12e260f1d07065
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-11-12 12:19:20 -0500

    adds missing specs for sizable utility module.

--- spec/lib/nfg_ui/bootstrap/utilities/sizable_spec.rb
+++ spec/lib/nfg_ui/bootstrap/utilities/sizable_spec.rb
@@ -12,59 +12,76 @@ RSpec.describe NfgUi::Bootstrap::Utilities::Sizable do
     it { is_expected.to eq tested_size }
   end
 
-  describe '#resized?' do
-    subject { button.send(:resized?) }
-    context 'when size is :sm' do
-      let(:tested_size) { :sm }
-      it { is_expected.to be }
-    end
+  describe 'private methods' do
+    describe '#css_classes' do
+      subject { button.send(:css_classes) }
 
-    context 'when size is :lg' do
-      let(:tested_size) { :lg }
-      it { is_expected.to be }
-    end
+      context 'when size is a valid size' do
+        context 'when size is :lg' do
+          let(:tested_size) { :lg }
+          it { is_expected.to eq 'btn btn-lg btn-primary' }
+        end
 
-    context 'when size is not :sm or :lg' do
-      let(:tested_size) { :invalid }
-      it { is_expected.to be false }
-    end
-
-    context 'when size is nil' do
-      let(:tested_size) { nil }
-
-      it { is_expected.to be_falsey }
-    end
-  end
-
-  describe '#non_html_attribute_options' do
-    subject { button.send(:non_html_attribute_options) }
-    it { is_expected.to include :size }
-  end
-
-  describe '#css_classes' do
-    subject { button.send(:css_classes) }
-
-    context 'when size is a valid size' do
-      context 'when size is :lg' do
-        let(:tested_size) { :lg }
-        it { is_expected.to eq 'btn btn-lg btn-primary' }
+        context 'when size is :sm' do
+          let(:tested_size) { :sm }
+          it { is_expected.to eq 'btn btn-sm btn-primary' }
+        end
       end
 
+      context 'when size is not a valid size' do
+        let(:tested_size) { :invalid }
+        it { is_expected.to eq 'btn btn-primary' }
+        it { is_expected.not_to include "btn-#{tested_size}" }
+      end
+
+      context 'when size is not present in options' do
+        let(:options) { {} }
+        it { is_expected.to eq 'btn btn-primary' }
+      end
+    end
+
+    describe '#non_html_attribute_options' do
+      subject { button.send(:non_html_attribute_options) }
+      it { is_expected.to include :size }
+    end
+
+    describe '#resized?' do
+      subject { button.send(:resized?) }
       context 'when size is :sm' do
         let(:tested_size) { :sm }
-        it { is_expected.to eq 'btn btn-sm btn-primary' }
+        it { is_expected.to be }
+      end
+
+      context 'when size is :lg' do
+        let(:tested_size) { :lg }
+        it { is_expected.to be }
+      end
+
+      context 'when size is not :sm or :lg' do
+        let(:tested_size) { :invalid }
+        it { is_expected.to be false }
+      end
+
+      context 'when size is nil' do
+        let(:tested_size) { nil }
+
+        it { is_expected.to be_falsey }
       end
     end
 
-    context 'when size is not a valid size' do
-      let(:tested_size) { :invalid }
-      it { is_expected.to eq 'btn btn-primary' }
-      it { is_expected.not_to include "btn-#{tested_size}" }
+    describe '#size_css_class' do
+      subject { button.send(:size_css_class) }
+      let(:options) { { size: :lg } }
+      it { is_expected.to eq 'btn-lg' }
     end
 
-    context 'when size is not present in options' do
-      let(:options) { {} }
-      it { is_expected.to eq 'btn btn-primary' }
+    describe '#size_css_class_prefix' do
+      subject { button.send(:size_css_class) }
+      let(:options) { { size: :lg } }
+      subject { button.send(:size_css_class_prefix) }
+      it 'uses the component css class as the prefix' do
+        expect(subject).to eq button.send(:component_css_class)
+      end
     end
   end
 end
