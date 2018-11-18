84bc6eb8bda6ca588ad78e278a68ce04fd856a4f
jonathanroehm <jonathan.roehm@networkforgood.com>
2018-11-17 20:38:55 -0500

    Updates slats to allow for non-menu based slat actions -- particularly for shopping carts (delete button only, etc).

--- lib/nfg_ui/components/patterns/slat_actions.rb
+++ lib/nfg_ui/components/patterns/slat_actions.rb
@@ -5,8 +5,18 @@ module NfgUi
     module Patterns
       # Slat doc coming soon
       class SlatActions < NfgUi::Components::Base
+        include Bootstrap::Utilities::Themeable
+
+        include NfgUi::Components::Utilities::Iconable
+
+        include NfgUi::Components::Traits::Theme
+
         def component_family
           :slats
+        end
+
+        def menu
+          options.fetch(:menu, true)
         end
 
         def slat_header
@@ -19,6 +29,18 @@ module NfgUi
 
         private
 
+        def default_theme
+          nil
+        end
+
+        def theme_css_class_prefix
+          'text-'
+        end
+
+        def outlineable?
+          false
+        end
+
         def css_classes
           [
             super,
@@ -27,7 +49,7 @@ module NfgUi
         end
 
         def non_html_attribute_options
-          super.push(:slat_header, :wide)
+          super.push(:slat_header, :wide, :menu)
         end
       end
     end
