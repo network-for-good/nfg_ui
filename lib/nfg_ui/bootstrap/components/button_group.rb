81feef93f8b0c364020174f910e2cc7d9c07a091
Andrew Porterfield <ajporterfield@gmail.com>
2018-11-30 09:29:00 -0500

    Converts button group

--- lib/nfg_ui/bootstrap/components/button_group.rb
+++ lib/nfg_ui/bootstrap/components/button_group.rb
@@ -14,6 +14,12 @@ module NfgUi
           options.fetch(:vertical, false)
         end
 
+        def render
+          content_tag(:div, html_options) do
+            (block_given? ? yield : body)
+          end
+        end
+
         private
 
         def assistive_html_attributes
