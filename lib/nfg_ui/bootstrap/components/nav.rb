30292701ecf5d65c4cc89c0d151ecc6509eb25e0
Andrew Porterfield <ajporterfield@gmail.com>
2018-12-01 12:45:02 -0500

    Converts nav

--- lib/nfg_ui/bootstrap/components/nav.rb
+++ lib/nfg_ui/bootstrap/components/nav.rb
@@ -33,6 +33,12 @@ module NfgUi
           options.fetch(:vertical, false)
         end
 
+        def render
+          content_tag(as, html_options) do
+            (block_given? ? yield : body)
+          end
+        end
+
         private
 
         def component_css_class
