71d32028e036c972a914a7244535ea4b9130c9b2
Andrew Porterfield <ajporterfield@gmail.com>
2018-12-03 14:52:43 -0500

    Fixed up table spec

--- spec/views/nfg_ui/bootstrap/_table.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/_table.html.haml_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'nfg_ui/bootstrap/_table.html.haml', type: :view do
   let(:body) { 'test body content' }
   let(:table) { FactoryBot.create(:bootstrap_table, body: body) }
-  subject { render 'nfg_ui/bootstrap/table', table: table }
+  subject { table.render }
 
   it 'outputs a bootstrap table component in html' do
     expect(subject).to have_selector '.table', text: body
-    expect(subject).to eq "<table class='table'>\n#{body}\n</table>\n"
+    expect(subject).to eq "<table class=\"table\">#{body}</table>"
   end
 end
