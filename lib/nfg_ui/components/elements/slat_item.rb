50e8d75611800139865f35b6b947a2cd3c9c27db
Andrew Porterfield <ajporterfield@gmail.com>
2018-12-03 09:07:41 -0500

    Converts slat body/item

--- lib/nfg_ui/components/elements/slat_item.rb
+++ lib/nfg_ui/components/elements/slat_item.rb
@@ -22,6 +22,27 @@ module NfgUi
           options.fetch(:caption, nil)
         end
 
+        def render
+          content_tag(:div, html_options) do
+            if slat_header
+              concat(content_tag(:h6, slat_header, class: 'display-4'))
+            end
+            if heading
+              if href
+                concat(link_to slat_item.href {
+                  NfgUi::Components::Foundations::Typeface.new({ subheading: heading }, view_context).render
+                })
+              else
+                concat(NfgUi::Components::Foundations::Typeface.new({ subheading: heading }, view_context).render)
+              end
+            end
+            concat(block_given? ? yield : body)
+            if caption
+              concat(NfgUi::Components::Foundations::Typeface.new({ caption: caption, class: 'mb-0' }, view_context).render)
+            end
+          end
+        end
+
         private
 
         def css_classes
