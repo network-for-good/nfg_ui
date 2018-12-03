7e995044af314f8357aa29c86beeba6f79fd04cb
Andrew Porterfield <ajporterfield@gmail.com>
2018-12-03 15:01:54 -0500

    Form, input_group specs

--- spec/views/nfg_ui/bootstrap/_input_group.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/_input_group.html.haml_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'nfg_ui/bootstrap/_input_group.html.haml', type: :view do
   let(:body) { 'test body content' }
   let(:input_group) { FactoryBot.create(:bootstrap_input_group, body: body) }
-  subject { render 'nfg_ui/bootstrap/input_group', input_group: input_group }
+  subject { input_group.render }
 
   it 'outputs a bootstrap input_group component in html' do
     expect(subject).to have_css '.input-group'
     expect(subject).to have_selector '.input-group', text: body
-    expect(subject).to eq "<div class='input-group'>\n#{body}\n</div>\n"
+    expect(subject).to eq "<div class=\"input-group\">#{body}</div>"
   end
 end
