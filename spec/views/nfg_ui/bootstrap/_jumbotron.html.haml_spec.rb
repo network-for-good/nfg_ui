d33d6db96889c7469030f96bbb6b7231b10213ba
Andrew Porterfield <ajporterfield@gmail.com>
2018-12-03 14:59:14 -0500

    Jumbotron spec

--- spec/views/nfg_ui/bootstrap/_jumbotron.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/_jumbotron.html.haml_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'nfg_ui/bootstrap/_jumbotron.html.haml', type: :view do
   let(:body) { 'test body content' }
   let(:jumbotron) { FactoryBot.create(:bootstrap_jumbotron, body: body) }
-  subject { render 'nfg_ui/bootstrap/jumbotron', jumbotron: jumbotron }
+  subject { jumbotron.render }
 
   it 'outputs a bootstrap jumbotron component in html' do
     expect(subject).to have_css '.jumbotron'
     expect(subject).to have_selector '.jumbotron', text: body
-    expect(subject).to eq "<div class='jumbotron'>\n#{body}\n</div>\n"
+    expect(subject).to eq "<div class=\"jumbotron\">#{body}</div>"
   end
 end
