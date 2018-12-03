9583f8c8e865846c11009d7b8db01055359a9386
Andrew Porterfield <ajporterfield@gmail.com>
2018-12-03 11:38:41 -0500

    Converts page header

--- lib/nfg_ui/components/patterns/page_header.rb
+++ lib/nfg_ui/components/patterns/page_header.rb
@@ -24,6 +24,53 @@ module NfgUi
           options.fetch(:subtitle, nil)
         end
 
+        # TODO: These resource_theme methods are just temporary placeholders.
+        # They need to come from ResourceThemeable somehow.
+        def resource_theme_icon
+          'heart-o'
+        end
+
+        def resource_theme_color
+          'primary'
+        end
+
+        def render
+          content_tag(:div, class: 'container-fluid', **html_options) do
+            content_tag(:div, class: 'row') do
+              content_tag(:div, class: 'col col-xl-11 mx-auto') do
+                content_tag(:div, class: 'row align-items-center') do
+                  concat(content_tag(:div, class: 'col py-2') {
+                    NfgUi::Components::Patterns::Media.new({}, view_context).render do
+                      capture do
+                        concat(NfgUi::Components::Elements::MediaObject.new({}, view_context).render {
+                          content_tag(:div, class: 'mr-2 h2') do
+                            NfgUi::Components::Foundations::Icon.new({ traits: [resource_theme_icon], class: "text-#{resource_theme_color} mr-0" }, view_context).render
+                          end
+                        })
+                        concat(NfgUi::Components::Elements::MediaBody.new({}, view_context).render {
+                          content_tag(:h2) do
+                            concat(title)
+                            if subtitle
+                              concat(content_tag(:small, subtitle, class: 'text-muted'))
+                            end
+                          end
+                        })
+                      end
+                    end
+                  })
+                  if body
+                    concat(content_tag(:div, class: 'col-12 col-md-auto py-2') {
+                      NfgUi::Components::Patterns::ButtonToolbar.new({ class: 'align-items-center' }, view_context).render do
+                        (block_given? ? yield : body)
+                      end
+                    })
+                  end
+                end
+              end
+            end
+          end
+        end
+
         private
 
         def non_html_attribute_options
