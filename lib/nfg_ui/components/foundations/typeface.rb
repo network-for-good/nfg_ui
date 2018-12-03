d2ef1dddc5a3f08407674dd5032f023847dd8f7f
Thomas Hoen <tom@givecorps.com>
2018-12-03 09:25:01 -0500

    conversion of foundation components

--- lib/nfg_ui/components/foundations/typeface.rb
+++ lib/nfg_ui/components/foundations/typeface.rb
@@ -16,6 +16,16 @@ module NfgUi
         include NfgUi::Components::Traits::Theme
         include NfgUi::Components::Traits::Typeface
 
+        def render
+          content_tag(as, **html_options) do
+            if icon
+              NfgUi::Components::Foundations::Icon.new({ icon: icon, text: body }, view_context).render
+            else
+              body
+            end
+          end
+        end
+
         def as
           if heading
             heading_tag
