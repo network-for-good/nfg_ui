3322be83eb57f153991ee07fb211a13461507310
Thomas Hoen <tom@givecorps.com>
2018-12-03 14:20:34 -0500

    uses component in partial spec and fixes spec to match component: breadcrumb-item

--- spec/views/nfg_ui/bootstrap/breadcrumbs/_breadcrumb_item.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/breadcrumbs/_breadcrumb_item.html.haml_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'nfg_ui/bootstrap/breadcrumbs/_breadcrumb_item.html.haml', type: :view do
   let(:breadcrumb_item) { FactoryBot.create(:bootstrap_breadcrumb_item, **options) }
   let(:options) { {} }
-  subject { render 'nfg_ui/bootstrap/breadcrumbs/breadcrumb_item', breadcrumb_item: breadcrumb_item }
+  subject { breadcrumb_item.render }
 
   describe 'html output for the bootstrap badge' do
     let(:options) { { body: body } }
@@ -12,7 +12,7 @@ RSpec.describe 'nfg_ui/bootstrap/breadcrumbs/_breadcrumb_item.html.haml', type: 
     it 'outputs a bootstrap breadcrumb component in html' do
       expect(subject).to have_css 'li.breadcrumb-item'
       expect(subject).to have_selector 'li.breadcrumb-item', text: body
-      expect(subject).to eq "<li class='breadcrumb-item'>\n#{body}\n</li>\n"
+      expect(subject).to eq "<li class=\"breadcrumb-item\">#{body}</li>"
     end
 
     describe 'active breadcrumb item' do
@@ -21,7 +21,7 @@ RSpec.describe 'nfg_ui/bootstrap/breadcrumbs/_breadcrumb_item.html.haml', type: 
         it 'outputs an active breadcrumb item' do
           expect(subject).to have_css '.breadcrumb-item.active'
           expect(subject).to have_css ".active[aria-current='page']"
-          expect(subject).to include "<li aria-current='page' class='breadcrumb-item active'>"
+          expect(subject).to include "<li class=\"breadcrumb-item active\" aria-current=\"page\">"
         end
       end
 
@@ -31,7 +31,7 @@ RSpec.describe 'nfg_ui/bootstrap/breadcrumbs/_breadcrumb_item.html.haml', type: 
           expect(subject).to have_css '.breadcrumb-item'
           expect(subject).not_to have_css '.active'
           expect(subject).not_to have_css "[aria-current]"
-          expect(subject).not_to include "aria-current='page", "class='breadcrumb-item active'"
+          expect(subject).not_to include "aria-current='page", 'class=\"breadcrumb-item active\"'
         end
       end
     end
