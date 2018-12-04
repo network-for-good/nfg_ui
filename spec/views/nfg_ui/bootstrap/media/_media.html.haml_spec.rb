6aa72340f0593b35f95d2ebc37b092161e24abf2
Andrew Porterfield <ajporterfield@gmail.com>
2018-12-04 08:58:06 -0500

    Converts media specs

--- spec/views/nfg_ui/bootstrap/media/_media.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/media/_media.html.haml_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'nfg_ui/bootstrap/media/_media.html.haml', type: :view do
   let(:body) { 'test body content' }
   let(:media) { FactoryBot.create(:bootstrap_media, body: body) }
-  subject { render 'nfg_ui/bootstrap/media/media', media: media }
+  subject { media.render }
 
   it 'outputs a bootstrap media component in html' do
     expect(subject).to have_css '.media'
     expect(subject).to have_selector '.media', text: body
-    expect(subject).to eq "<div class='media'>\n#{body}\n</div>\n"
+    expect(subject).to eq "<div class=\"media\">#{body}</div>"
   end
 end
--- spec/views/nfg_ui/bootstrap/media/_media_body.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/media/_media_body.html.haml_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'nfg_ui/bootstrap/media/_media_body.html.haml', type: :view do
   let(:body) { 'test body content' }
   let(:media_body) { FactoryBot.create(:bootstrap_media_body, body: body) }
-  subject { render 'nfg_ui/bootstrap/media/media_body', media_body: media_body }
+  subject { media_body.render }
 
   it 'outputs a bootstrap media component in html' do
     expect(subject).to have_css '.media-body'
     expect(subject).to have_selector '.media-body', text: body
-    expect(subject).to eq "<div class='media-body'>\n#{body}\n</div>\n"
+    expect(subject).to eq "<div class=\"media-body\">#{body}</div>"
   end
 end
