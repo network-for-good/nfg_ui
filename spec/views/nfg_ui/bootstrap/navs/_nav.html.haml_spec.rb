71b67fc9a1f4a2635fe46980b36ad8306fbba265
Andrew Porterfield <ajporterfield@gmail.com>
2018-12-04 08:50:18 -0500

    Converted nav specs

--- spec/views/nfg_ui/bootstrap/navs/_nav.html.haml_spec.rb
+++ spec/views/nfg_ui/bootstrap/navs/_nav.html.haml_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe 'nfg_ui/bootstrap/navs/_nav.html.haml', type: :view do
   let(:body) { 'test body content' }
   let(:nav) { FactoryBot.create(:bootstrap_nav, body: body, **options) }
   let(:options) { {} }
-  subject { render 'nfg_ui/bootstrap/navs/nav', nav: nav }
+  subject { nav.render }
 
   it 'outputs a bootstrap nav component in html' do
     expect(subject).to have_selector 'ul.nav', text: body
-    expect(subject).to eq "<ul class=\"nav\">#{body}\n</ul>"
+    expect(subject).to eq "<ul class=\"nav\">#{body}</ul>"
   end
 
   describe 'horizontal nav alignment' do
@@ -16,7 +16,7 @@ RSpec.describe 'nfg_ui/bootstrap/navs/_nav.html.haml', type: :view do
       let(:options) { { center: true } }
       it 'outputs a centered nav' do
         expect(subject).to have_css '.nav.justify-content-center'
-        expect(subject).to eq "<ul class=\"nav justify-content-center\">#{body}\n</ul>"
+        expect(subject).to eq "<ul class=\"nav justify-content-center\">#{body}</ul>"
       end
     end
 
@@ -24,7 +24,7 @@ RSpec.describe 'nfg_ui/bootstrap/navs/_nav.html.haml', type: :view do
       let(:options) { { right: true } }
       it 'outputs a right aligned nav' do
         expect(subject).to have_css '.nav.justify-content-end'
-        expect(subject).to eq "<ul class=\"nav justify-content-end\">#{body}\n</ul>"
+        expect(subject).to eq "<ul class=\"nav justify-content-end\">#{body}</ul>"
       end
     end
   end
@@ -33,7 +33,7 @@ RSpec.describe 'nfg_ui/bootstrap/navs/_nav.html.haml', type: :view do
     let(:options) { { vertical: true } }
     it 'outputs a vertical nav' do
       expect(subject).to have_css '.nav.flex-column'
-      expect(subject).to eq "<ul class=\"nav flex-column\">#{body}\n</ul>"
+      expect(subject).to eq "<ul class=\"nav flex-column\">#{body}</ul>"
     end
   end
 
@@ -42,7 +42,7 @@ RSpec.describe 'nfg_ui/bootstrap/navs/_nav.html.haml', type: :view do
     let(:options) { { as: tested_element } }
     it 'outputs a nav utilizing a custom wrapper element' do
       expect(subject).to have_css "#{tested_element}.nav"
-      expect(subject).to eq "<#{tested_element} class=\"nav\">#{body}\n</#{tested_element}>"
+      expect(subject).to eq "<#{tested_element} class=\"nav\">#{body}</#{tested_element}>"
     end
   end
 
@@ -50,7 +50,7 @@ RSpec.describe 'nfg_ui/bootstrap/navs/_nav.html.haml', type: :view do
     let(:options) { { fill: true } }
     it 'outputs a nav with fill' do
       expect(subject).to have_css '.nav.nav-fill'
-      expect(subject).to eq "<ul class=\"nav nav-fill\">#{body}\n</ul>"
+      expect(subject).to eq "<ul class=\"nav nav-fill\">#{body}</ul>"
     end
   end
 
@@ -58,7 +58,7 @@ RSpec.describe 'nfg_ui/bootstrap/navs/_nav.html.haml', type: :view do
     let(:options) { { justified: true } }
     it 'outputs a justified nav' do
       expect(subject).to have_css '.nav.nav-justified'
-      expect(subject).to eq "<ul class=\"nav nav-justified\">#{body}\n</ul>"
+      expect(subject).to eq "<ul class=\"nav nav-justified\">#{body}</ul>"
     end
   end
 
@@ -66,7 +66,7 @@ RSpec.describe 'nfg_ui/bootstrap/navs/_nav.html.haml', type: :view do
     let(:options) { { tabs: true } }
     it 'outputs a nav with tabs' do
       expect(subject).to have_css '.nav.nav-tabs'
-      expect(subject).to eq "<ul class=\"nav nav-tabs\">#{body}\n</ul>"
+      expect(subject).to eq "<ul class=\"nav nav-tabs\">#{body}</ul>"
     end
   end
 
@@ -74,7 +74,7 @@ RSpec.describe 'nfg_ui/bootstrap/navs/_nav.html.haml', type: :view do
     let(:options) { { pill: true } }
     it 'outputs a nav with tabs' do
       expect(subject).to have_css '.nav.nav-pills'
-      expect(subject).to eq "<ul class=\"nav nav-pills\">#{body}\n</ul>"
+      expect(subject).to eq "<ul class=\"nav nav-pills\">#{body}</ul>"
     end
   end
 end
