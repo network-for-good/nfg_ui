9350cfa19b0840899c1c4432913b2e28a90b4485
Thomas Hoen <tom@givecorps.com>
2018-12-10 18:04:30 -0500

    Modifies slat actions to place a data-method attribute on the link when the method passed in is delete

--- lib/nfg_ui/components/patterns/slat_actions.rb
+++ lib/nfg_ui/components/patterns/slat_actions.rb
@@ -22,7 +22,12 @@ module NfgUi
         end
 
         def method
+          return if delete_link?
           options.fetch(:method, nil)
+        end
+
+        def delete_link?
+          @delete_link ||= options.fetch(:method, nil) == :delete
         end
 
         def slat_header
@@ -50,7 +55,7 @@ module NfgUi
             method: send(:method),
             remote: remote,
             data: { disable_with: disable_with,
-                    confirm: confirm } }
+                    confirm: confirm }.merge(delete_link? ? { method: :delete } : {}) }
         end
 
         def theme
