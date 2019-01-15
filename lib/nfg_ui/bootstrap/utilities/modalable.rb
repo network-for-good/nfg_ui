0d901ccffb9dfd9b1571f0d0f3031a7b76cde56b
jonathanroehm <jonathan.roehm@networkforgood.com>
2019-01-15 11:41:59 -0500

    Updates notes & adds examples with explanations.

--- lib/nfg_ui/bootstrap/utilities/modalable.rb
+++ lib/nfg_ui/bootstrap/utilities/modalable.rb
@@ -3,12 +3,27 @@
 module NfgUi
   module Bootstrap
     module Utilities
-      # Delivers the dismissibility of the component to the HAML partial
+      # Allows a component to utilize the :modal option
+      # Which then automatically formats the component's data attributes
+      # to connect to the desired modal. Note the modal option requires the '#'
+      # per bootstrap docs.
+      #
+      # Example usage:
+      # ui.nfg(:button, body: 'Modal Button', modal: '#the_target_modal_id')
+      #
+      # Will now activate this modal:
+      # ui.nfg(:modal, body: 'The modal', id: 'the_target_modal_id')
       module Modalable
         def modal
           options.fetch(:modal, nil)
         end
 
+        # Only overwrite data-toggle and data-target
+        # The operating assumption is that activating a modal is more important
+        # and thus, will take precedence over less valuable competing data toggles
+        #
+        # For example: ui.nfg(:button, modal: '#the_target_modal', tooltip: 'The Tooltip')
+        # will ignore the tooltip (since tooltip is initialized via a competing data-toggle)
         def data
           modal ? super.merge!(toggle: 'modal', target: options[:modal]) : super
         end
