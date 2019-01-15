d9ede06ca75cae94d14d8af661987e029403ca90
jonathanroehm <jonathan.roehm@networkforgood.com>
2019-01-15 15:26:30 -0500

    Updates notes per recommendations regarding clarifying remote modals.

--- lib/nfg_ui/bootstrap/utilities/modalable.rb
+++ lib/nfg_ui/bootstrap/utilities/modalable.rb
@@ -6,13 +6,27 @@ module NfgUi
       # Allows a component to utilize the :modal option
       # Which then automatically formats the component's data attributes
       # to connect to the desired modal. Note the modal option requires the '#'
-      # per bootstrap docs.
+      # preceding the ID per bootstrap docs ('#the_modal_id' not 'the_modal_id')
+      # 
+      # Usage notes:
+      # 1. When to use:
+      # The :modal option should only be used when calling to a modal
+      # that has been embedded on the page (not for remote links, more below).
+      # There are no detrimental effects to using the modal option on a 
+      # component that does not have a corresponding modal, however it
+      # does add useless data attributes to your component.
+      # 
+      # 2. Regarding ajax remotely rendered modals
+      # When leveraging a :remote link that renders a modal remotely,
+      # the component does _not_ need the modal option. Your action's js.erb
+      # will/should render the partial within the modal as part of its javascript.
+      # 
+      # Example usage for (non-remote) embedded modal:
+      # The modalable component:
+      # ui.bootstrap(:button, body: 'Modal Button', modal: '#the_target_modal_id')
       #
-      # Example usage:
-      # ui.nfg(:button, body: 'Modal Button', modal: '#the_target_modal_id')
-      #
-      # Will now activate this modal:
-      # ui.nfg(:modal, body: 'The modal', id: 'the_target_modal_id')
+      # The actual modal to be activated, embedded on the page:
+      # ui.bootstrap(:modal, body: 'The modal', id: 'the_target_modal_id')
       module Modalable
         def modal
           options.fetch(:modal, nil)
