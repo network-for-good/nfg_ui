8ef7cc1e8d26b6aadb89fbd4cede2c854678c30c
jonathanroehm <jonathan.roehm@networkforgood.com>
2019-01-23 11:40:39 -0500

    removes this special conditional logic for css_classes on dropdown_items. Apparently syntax and setup has changed for bootstrap 4.2 (or this was never implemented correctly)

--- lib/nfg_ui/bootstrap/components/dropdown_item.rb
+++ lib/nfg_ui/bootstrap/components/dropdown_item.rb
@@ -32,20 +32,6 @@ module NfgUi
 
         private
 
-        # When a dropdown item is both disabled AND given a tooltip
-        # We need to restructure the syntax to wrap the dropdown item in
-        # a faux dropdown item that also contains the tooltip content
-        #
-        # This is similar to disabled, tooltipped buttons but works to maintain the UI
-        # of the dropdown menu
-        def css_classes
-          if disabled && tooltip.present?
-            super.gsub(component_css_class, '').gsub('disabled', '')
-          else
-            super
-          end
-        end
-
         def default_html_wrapper_element
           :a
         end
