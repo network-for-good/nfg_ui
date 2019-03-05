34c66dc429a2990d4076141b61dcd3a243f8c640
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-03-05 15:19:41 -0500

    adds spec to validate that pb-3 is present on the inner carousel. If we take this out, we'll know it was removed because of the failing spec. This is a unique implementation of the carousel-inner area, counter to its use on BS4 (given that our carousels are for text content and not images), making sure we recognize this change and notice if it's removed or changed.

--- spec/lib/nfg_ui/components/patterns/carousel_spec.rb
+++ spec/lib/nfg_ui/components/patterns/carousel_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe NfgUi::Components::Patterns::Carousel do
   let(:carousel) { described_class.new(options, ActionController::Base.new.view_context) }
+  let(:rendered) { carousel.render }
   let(:options) { {} }
   it { expect(described_class).to be < NfgUi::Bootstrap::Components::Carousel }
   it_behaves_like 'a component with a consistent initalized construction'
@@ -9,4 +10,14 @@ RSpec.describe NfgUi::Components::Patterns::Carousel do
   it_behaves_like 'a component that includes the Renderable utility module'
 
   it { expect(described_class.included_modules).to include NfgUi::Components::Utilities::Traitable }
+
+
+  describe '#render' do
+    subject { Capybara.string(rendered) }
+
+    it 'includes a unique padding on the inner carousel' do
+      expect(subject).to have_css '.carousel-inner.pb-3'
+    end
+
+  end
 end
