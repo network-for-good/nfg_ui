e5bc963fa69f81e6fcf73c60c7cafea13ed211b6
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-03-15 13:27:30 -0400

    another futile attempt, before going brute force.

--- lib/nfg_ui/engine.rb
+++ lib/nfg_ui/engine.rb
@@ -7,8 +7,8 @@ module NfgUi
     config.autoload_paths << Engine.root.join("lib")
 
 
-    initializer :assets do |app|
-      config.assets.paths << Engine.root.join("app", "assets", "images", "nfg_ui")
+    initializer "nfg_ui.assets.precompile" do |app|
+      Rails.application.config.assets.paths << Engine.root.join('app', 'assets', 'images', 'nfg_ui' )
     end
 
     config.to_prepare do
