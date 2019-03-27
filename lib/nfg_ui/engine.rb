57572275cbb3ec3729a1310e2d492535367da747
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-03-27 10:08:28 -0400

    The first attempt, without file extensions listed did not work (Sprockets error in host app: Asset was not declared to be precompiled in production.). This attempt is explicitly defining the file type extension.

--- lib/nfg_ui/engine.rb
+++ lib/nfg_ui/engine.rb
@@ -6,12 +6,19 @@ module NfgUi
 
     config.autoload_paths << Engine.root.join("lib")
 
-    # TODO: Could not get asset paths to precompile the images/nfg_ui
-    # folder. For now, brute force all images via Dir.glob...
+    # In the event that we need to pre-compile assets, this operates
+    # as a working example of pre-compiling the assets/images folder
+    # for consumption and use by the host app.
+    #
+    # Example: (from within host app)
+    # = image_tag 'nfg_ui/email/logo.png'
+    #
+    # Noted by JR: 3/27/19
+    # Remove this note when appropriate.
+    #
     # initializer "nfg_ui.assets.precompile" do |app|
     #   # assets = Dir.glob(Engine.root.join('app', 'assets', 'images', 'nfg_ui', '**', '*'))
     #   # assets << "#{Engine.root.join('public')}/favicon.ico"
-
     #   # app.config.assets.precompile += assets
     # end
 
