7002b428d7a716be93eecdabe63e3743d781de72
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-03-27 10:31:22 -0400

    After some testing. It appears that we can link_tree as long as we precompile the manifest.js. This works locally for the test_app

--- app/assets/config/nfg_ui_manifest.js
+++ app/assets/config/nfg_ui_manifest.js
@@ -1,6 +1,3 @@
 //= link_directory ../javascripts/nfg_ui .js
 //= link_directory ../stylesheets/nfg_ui .css
-//= link_tree ../images/nfg_ui .png
-//= link_tree ../images/nfg_ui .jpg
-//= link_tree ../images/nfg_ui .xml
-//= link_tree ../images/nfg_ui .webmanifest
+//= link_tree ../images/nfg_ui
--- lib/nfg_ui/engine.rb
+++ lib/nfg_ui/engine.rb
@@ -6,21 +6,26 @@ module NfgUi
 
     config.autoload_paths << Engine.root.join("lib")
 
-    # In the event that we need to pre-compile assets, this operates
-    # as a working example of pre-compiling the assets/images folder
-    # for consumption and use by the host app.
-    #
-    # Example: (from within host app)
-    # = image_tag 'nfg_ui/email/logo.png'
-    #
-    # Noted by JR: 3/27/19
-    # Remove this note when appropriate.
-    #
-    # initializer "nfg_ui.assets.precompile" do |app|
-    #   # assets = Dir.glob(Engine.root.join('app', 'assets', 'images', 'nfg_ui', '**', '*'))
-    #   # assets << "#{Engine.root.join('public')}/favicon.ico"
-    #   # app.config.assets.precompile += assets
-    # end
+    initializer "nfg_ui.assets.precompile" do |app|
+      # In the event that we need to pre-compile assets, this operates
+      # as a working example of pre-compiling the assets/images folder
+      # for consumption and use by the host app.
+      #
+      # Example: (from within host app)
+      # = image_tag 'nfg_ui/email/logo.png'
+      #
+      # Noted by JR: 3/27/19
+      # Remove this note when appropriate.
+      #
+      # Code to re-enstate:
+      # assets = Dir.glob(Engine.root.join('app', 'assets', 'images', 'nfg_ui', '**', '*'))
+      # assets << "#{Engine.root.join('public')}/favicon.ico"
+      # app.config.assets.precompile += assets
+
+      app.config.assets.precompile << "#{Engine.root.join('app', 'assets', 'config')}/nfg_ui_manifest.js"
+    end
+
+
 
     config.to_prepare do
       ActiveSupport.on_load :action_controller do
