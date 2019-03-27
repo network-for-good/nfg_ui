b6ad1823ac8b1f0a55ee14ad04edd246138f941a
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-03-27 17:12:21 -0400

    Assets didn't make it into UAT; seeing if precomp is necessary

--- lib/nfg_ui/engine.rb
+++ lib/nfg_ui/engine.rb
@@ -29,6 +29,18 @@ module NfgUi
       app.config.assets.precompile << "#{Engine.root.join('app', 'assets', 'config')}/nfg_ui_manifest.js"
     end
 
+    # Merge `nfg_ui/public` folder's assets & files into
+    # the host app's `/public` folder.
+    #
+    # This is needed for situations where a file must be in
+    # the root directory of the website
+    #
+    # ex: favicons & Apple touch icons need to be at http://website.com/favicon.ico, etc.
+    # ... `public/favicon.ico` or `public/apple-touch-icon.png`
+    initializer "static assets" do |app|
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, Engine.root.join('public'))
+    end
+
     config.to_prepare do
       ActiveSupport.on_load :action_controller do
         helper NfgUi::ApplicationHelper
