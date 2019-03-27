76b55e469d32c49dfed141f0710adf5b6ecc596b
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-03-27 19:57:22 -0400

    some misc updates after reading further docs on Railtie. Fascinating stuff.

--- lib/nfg_ui/railtie.rb
+++ lib/nfg_ui/railtie.rb
@@ -1,10 +1,19 @@
+# frozen_string_literal: true
+
+require 'nfg_ui'
+require 'rails'
+
 module NfgUi
+  # Ensure that files in the public folder
+  # are successfully added during production asset:precompile
   class Railtie < Rails::Railtie
+    config.eager_load_namespaces << NfgUi
+
     config.before_initialize do
       version = Rails.version
-      if version >= "5.0.0"
+      if version >= '5.0.0'
         ::Rails.configuration.public_file_server.enabled = true
-      elsif version >= "4.2.0"
+      elsif version >= '4.2.0'
         ::Rails.configuration.serve_static_files = true
       else
         ::Rails.configuration.serve_static_assets = true
