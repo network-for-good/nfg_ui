62c9f7dac6afe0bc5fad99c9cad670a91ef2c5e1
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-03-26 22:57:04 -0400

    REmoves debug print and updates flakey spec issues by sleeping for a half second on dropdown click.

--- spec/support/helpers/dropdown.rb
+++ spec/support/helpers/dropdown.rb
@@ -9,4 +9,5 @@
 def maybe_open_dropdown_menu(maybe_open_dropdown:, dropdown_data_describe: 'dropdown-menu', nested_toggle: false)
   page.find('body').click # reset dropdown
   page.find("[data-describe='#{dropdown_data_describe}']#{' .dropdown-toggle' if nested_toggle}").click if maybe_open_dropdown
+  sleep 0.5
 end
