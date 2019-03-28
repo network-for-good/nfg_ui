c44ff7eeb10031b8814b16e3ed77a26c50c86a37
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-03-27 22:45:46 -0400

    Adds a rake task to simply copy the public files over. Interesting idea.

--- lib/nfg_ui/engine.rb
+++ lib/nfg_ui/engine.rb
@@ -29,21 +29,6 @@ module NfgUi
       app.config.assets.precompile << "#{Engine.root.join('app', 'assets', 'config')}/nfg_ui_manifest.js"
     end
 
-    # Merge `nfg_ui/public` folder's assets & files into
-    # the host app's `/public` folder.
-    #
-    # This is needed for situations where a file must be in
-    # the root directory of the website
-    #
-    # ex: favicons & Apple touch icons need to be at http://website.com/favicon.ico, etc.
-    # ... `public/favicon.ico` or `public/apple-touch-icon.png`
-    #
-    # This is supported by also including the `public` folder in the gemspec's files list:
-    # s.files = Dir['{app,config,db,lib,public}/**/*', 'MIT-LICENSE', 'Rakefile', 'README.md']
-    initializer "static assets" do |app|
-      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
-    end
-
     config.to_prepare do
       ActiveSupport.on_load :action_controller do
         helper NfgUi::ApplicationHelper
--- lib/tasks/nfg_ui_tasks.rake
+++ lib/tasks/nfg_ui_tasks.rake
@@ -2,3 +2,18 @@
 # task :nfg_ui do
 #   # Task goes here
 # end
+
+Rake::Task['assets:precompile'].enhance do
+  Rake::Task['nfg_ui:copy_public_assets'].invoke
+end
+
+namespace :nfg_ui do
+  task :copy_public_assets => :"assets:environment"  do
+    assets = ['favicon.ico', 'apple-touch-icon.png']
+    assets.each do |asset|
+      source_file = File.join(MyEngine::Engine.root, 'public', asset)
+      dest_file = File.join(Rails.root, 'public', asset)
+      FileUtils.copy_file source_file, dest_file, true
+    end
+  end
+end
