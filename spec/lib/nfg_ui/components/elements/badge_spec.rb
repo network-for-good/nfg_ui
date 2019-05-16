d6ca78867e62541ced035312430ff942b363df6c
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-05-16 11:40:52 -0400

    adds tooltips to badges. Updates specs.

--- spec/lib/nfg_ui/components/elements/badge_spec.rb
+++ spec/lib/nfg_ui/components/elements/badge_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe NfgUi::Components::Elements::Badge do
   let(:badge) { described_class.new(options, ActionController::Base.new.view_context) }
-  let(:options) { {} } 
+  let(:options) { {} }
   it { expect(described_class).to be < NfgUi::Bootstrap::Components::Badge }
   it_behaves_like 'a component with a consistent initalized construction'
   it_behaves_like 'a component that includes the Describable utility module'
   it_behaves_like 'a component that includes the Renderable utility module'
-  
+  it_behaves_like 'a component that includes the Tooltipable utility module'
+
   it_behaves_like 'a component that includes the Theme trait module'
   it_behaves_like 'a component that includes the Pill trait module'
 
