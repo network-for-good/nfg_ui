b75acc2e4204be32cf61dfc7df367249586feb38
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-07-09 14:09:30 -0400

    moves button_group view spec into #render method spec for button_group_spec

--- spec/lib/nfg_ui/bootstrap/components/button_group_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/button_group_spec.rb
@@ -11,6 +11,101 @@ RSpec.describe NfgUi::Bootstrap::Components::ButtonGroup do
   it_behaves_like 'a component that includes the Sizable utility module', component_suite: :bootstrap
   it_behaves_like 'a component that includes the DropdownDirectionable utility module'
 
+  describe '#render' do
+    let(:rendered_html) { button_group.render }
+    let(:options) { {} }
+    subject { Capybara.string(rendered_html) }
+
+    describe 'html output for the bootstrap button_group' do
+      let(:options) { { body: body } }
+      let(:body) { 'test body content' }
+
+      it 'outputs a bootstrap button_group component in html' do
+        expect(subject).to have_css '.btn-group'
+        expect(subject).to have_selector '.btn-group', text: body
+        expect(rendered_html).to eq "<div class=\"btn-group\" role=\"group\" aria-label=\"action buttons\">#{body}</div>"
+      end
+    end
+
+    describe 'dropdown menu directions when acting as a container for a dropdown menu' do
+      describe 'dropdown menu with dropleft direction' do
+        context 'when dropleft is true in options' do
+          let(:options) { { dropleft: true } }
+
+          it 'outputs a dropdown component with dropleft html in place' do
+            expect(subject).to have_css '.btn-group.dropleft'
+            expect(rendered_html).to eq "<div class=\"btn-group dropleft\" role=\"group\" aria-label=\"action buttons\"></div>"
+          end
+        end
+
+        context 'when dropleft is false in options' do
+          let(:options) { { dropleft: false } }
+          it 'outputs a dropdown component without dropleft html in place' do
+            expect(subject).not_to have_css '.btn-group.dropleft'
+            expect(subject).to have_css '.btn-group'
+            expect(rendered_html).to eq "<div class=\"btn-group\" role=\"group\" aria-label=\"action buttons\"></div>"
+          end
+        end
+
+        context 'when dropleft is not present in options' do
+          let(:options) { {} }
+          it { is_expected.not_to have_css '.btn-group.dropleft' }
+          it { is_expected.to have_css '.btn-group' }
+        end
+      end
+
+      describe 'dropdown menu with dropright direction' do
+        context 'when dropright is true in options' do
+          let(:options) { { dropright: true } }
+          it 'outputs a dropdown component with dropright html in place' do
+            expect(subject).to have_css '.btn-group.dropright'
+            expect(rendered_html).to eq "<div class=\"btn-group dropright\" role=\"group\" aria-label=\"action buttons\"></div>"
+          end
+        end
+
+        context 'when dropright is false in options' do
+          let(:options) { { dropright: false } }
+          it 'outputs a dropdown component without dropright html in place' do
+            expect(subject).not_to have_css '.btn-group.dropright'
+            expect(subject).to have_css '.btn-group'
+            expect(rendered_html).to eq "<div class=\"btn-group\" role=\"group\" aria-label=\"action buttons\"></div>"
+          end
+        end
+
+        context 'when dropright is not present in options' do
+          let(:options) { {} }
+          it { is_expected.not_to have_css '.btn-group.dropright' }
+          it { is_expected.to have_css '.btn-group' }
+        end
+      end
+
+      describe 'dropdown menu with dropup direction' do
+        context 'when dropup is true in options' do
+          let(:options) { { dropup: true } }
+          it 'outputs a dropdown component with dropup html in place' do
+            expect(subject).to have_css '.btn-group.dropup'
+            expect(rendered_html).to eq "<div class=\"btn-group dropup\" role=\"group\" aria-label=\"action buttons\"></div>"
+          end
+        end
+
+        context 'when dropup is false in options' do
+          let(:options) { { dropup: false } }
+          it 'outputs a dropdown component without dropup html in place' do
+            expect(subject).not_to have_css '.btn-group.dropup'
+            expect(subject).to have_css '.btn-group'
+            expect(rendered_html).to eq "<div class=\"btn-group\" role=\"group\" aria-label=\"action buttons\"></div>"
+          end
+        end
+
+        context 'when dropup is not present in options' do
+          let(:options) { {} }
+          it { is_expected.not_to have_css '.btn-group.dropup' }
+          it { is_expected.to have_css '.btn-group' }
+        end
+      end
+    end
+  end
+
   describe '#vertical' do
     let(:options) { { vertical: tested_vertical } }
     let(:tested_vertical) { nil }
