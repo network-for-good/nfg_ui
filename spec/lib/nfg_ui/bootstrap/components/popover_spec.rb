860f81748bcbc798ceb71231105ac4be41f123af
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-07-09 16:12:05 -0400

    removes popover view spec, moves it to render spec.

--- spec/lib/nfg_ui/bootstrap/components/popover_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/popover_spec.rb
@@ -4,5 +4,8 @@ RSpec.describe NfgUi::Bootstrap::Components::Popover do
   subject { described_class }
   it { is_expected.to be < NfgUi::Bootstrap::Components::Base }
   it_behaves_like 'a component with a consistent initalized construction'
-  
+
+  describe '#render' do
+    it_behaves_like 'a simple rendered component with a body'
+  end
 end
