c13cbf89c9f083f649661ac5d7f2536650cab8ce
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-07-09 15:37:02 -0400

    Updates form_spec to use the shared example.

--- spec/lib/nfg_ui/bootstrap/components/form_spec.rb
+++ spec/lib/nfg_ui/bootstrap/components/form_spec.rb
@@ -9,15 +9,6 @@ RSpec.describe NfgUi::Bootstrap::Components::Form do
   it_behaves_like 'a component with a consistent initalized construction'
 
   describe '#render' do
-    let(:rendered_html) { form.render }
-    let(:options) { { body: body } }
-    let(:body) { 'test body' }
-    subject { Capybara.string(rendered_html) }
-
-    it 'outputs a bootstrap form component in html' do
-      expect(subject).to have_css '.form'
-      expect(subject).to have_selector '.form', text: body
-      expect(rendered_html).to eq "<div class=\"form\">#{body}</div>"
-    end
+    it_behaves_like 'a simple rendered component with a body'
   end
 end
