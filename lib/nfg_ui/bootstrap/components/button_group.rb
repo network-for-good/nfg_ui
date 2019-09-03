2eb23cbd248d115fd9914a3a86a38827641505bd
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-09-03 16:56:33 -0400

    Adds notes

--- lib/nfg_ui/bootstrap/components/button_group.rb
+++ lib/nfg_ui/bootstrap/components/button_group.rb
@@ -16,12 +16,15 @@ module NfgUi
         private
 
         def assistive_html_attributes
+          # Prevent overwriting supplied aria attributes in the component's :options
           aria = options[:aria].present? ? options[:aria] : {}
           aria.merge!(label: 'action buttons')
 
           super.merge!(role: 'group', aria: aria)
         end
 
+        # Bootstrap does not stack btn-group and btn-group-vertical together
+        # When a button group is set as vertical, it is only `btn-group-vertical`
         def component_css_class
           "btn-group#{'-vertical' if vertical}"
         end
