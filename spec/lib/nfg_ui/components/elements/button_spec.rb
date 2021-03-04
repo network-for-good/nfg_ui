82d915d1556512fb2788b9ee7611114e8421ab8d
Andrew Porterfield <ajporterfield@gmail.com>
2021-03-04 18:03:55 -0500

    DM-8192 Omits margin left class from icon if there's no body/block

--- spec/lib/nfg_ui/components/elements/button_spec.rb
+++ spec/lib/nfg_ui/components/elements/button_spec.rb
@@ -14,5 +14,35 @@ RSpec.describe NfgUi::Components::Elements::Button do
 
   it { expect(described_class.included_modules).to include NfgUi::Components::Utilities::Traitable }
 
-  pending 'button spec needs specs'
+  describe '#render' do
+    let(:rendered_html) { button.render }
+
+    context 'when icon is passed' do
+      let(:icon) { 'close' }
+      let(:body) { nil }
+      let(:options) {{ icon: icon, body: body }}
+
+      context 'when body is blank and no block is used' do
+        it "doesn't include an ml-1 class on the i tag" do
+          expect(rendered_html).to eq "<a class=\"btn btn-primary\" href=\"#\"><i aria-hidden=\"true\" class=\"fa fa-#{icon}\"></i></a>"
+        end
+      end
+
+      context 'when body is present' do
+        let(:body) { 'Cancel' }
+
+        it "includes an ml-1 class on the i tag" do
+          expect(rendered_html).to eq "<a class=\"btn btn-primary\" href=\"#\">#{body}<i aria-hidden=\"true\" class=\"fa fa-#{icon} ml-1\"></i></a>"
+        end
+      end
+
+      context 'when a block is used' do
+        let(:rendered_html) { button.render { 'Block' }}
+
+        it "includes an ml-1 class on the i tag" do
+          expect(rendered_html).to eq "<a class=\"btn btn-primary\" href=\"#\">Block<i aria-hidden=\"true\" class=\"fa fa-#{icon} ml-1\"></i></a>"
+        end
+      end
+    end
+  end
 end
