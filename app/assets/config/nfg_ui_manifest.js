451763be6e0489145675c47575f3afd097bfee28
Thomas Hoen <tom@givecorps.com>
2021-10-18 16:02:56 -0400

    adds the email application css file to the manifest

--- app/assets/config/nfg_ui_manifest.js
+++ app/assets/config/nfg_ui_manifest.js
@@ -1,3 +1,4 @@
 //= link_directory ../javascripts/nfg_ui .js
 //= link_directory ../stylesheets/nfg_ui .css
+//= link_directory ../stylesheets/nfg_ui/network_for_good/email .css
 //= link_tree ../images/nfg_ui
