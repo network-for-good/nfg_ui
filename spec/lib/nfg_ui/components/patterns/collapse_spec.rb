ec1100ec4e40627d856006d1fe20739c6d26ed3a
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2022-11-04 14:07:08 -0400

    Update collapse with a header to supply a random css ID. can also pass an icon into the heading button if you want. Adds specs

--- spec/lib/nfg_ui/components/patterns/collapse_spec.rb
+++ spec/lib/nfg_ui/components/patterns/collapse_spec.rb
@@ -13,5 +13,84 @@ RSpec.describe NfgUi::Components::Patterns::Collapse do
 
   it { expect(described_class.included_modules).to include NfgUi::Components::Utilities::Traitable }
 
-  pending 'collapse needs spec'
+  describe '#component_initialize' do
+    let(:test_id) { nil }
+    let(:test_heading) { nil }
+    let(:options) { { id: test_id, heading: test_heading } }
+
+    context 'when heading is not present in options' do
+      let(:test_heading) { nil }
+
+      context 'when an ID is not present' do
+        let(:test_id) { nil }
+
+        it 'does not generate an ID' do
+          expect(collapse.options[:id]).to be_nil
+        end
+      end
+
+      context 'when an ID is present' do
+        let(:test_id) { 'test-id' }
+
+        it 'does uses the options :ID' do
+          expect(collapse.options[:id]).to eq test_id
+        end
+      end
+    end
+
+    context 'when heading is present in options' do
+      let(:test_heading) { 'test heading' }
+
+      context 'when options includes an :id' do
+        let(:test_id) { 'test-id' }
+        it 'passes the id in options into the component' do
+          expect(collapse.options[:id]).to eq test_id
+        end
+      end
+
+      context 'when options does not include an :id' do
+        let(:test_id) { nil }
+        it 'supplies an :id' do
+          # it's a random set of letters so we can't really test the exact value
+          # but checking that ID is present when it's set to nil
+          # is good enough.
+          expect(Capybara.string(collapse.render)).to have_css "[id]"
+          expect(collapse.options[:id]).to be_present
+        end
+      end
+    end
+  end
+
+  describe '#render' do
+    let(:options) { { heading: test_heading, id: test_id } }
+    let(:test_heading) { nil }
+    let(:test_id) { nil }
+    let(:capybara_component) { Capybara.string(collapse.render) }
+
+    context 'when a :heading is supplied' do
+      let(:test_heading) { 'Test heading' }
+
+      context 'when an ID is not supplied' do
+        let(:test_id) { nil }
+
+        it 'renders a button and the collapse icon and a random ID' do
+          random_id = capybara_component.find('a')['aria-controls']
+          expect(capybara_component).to have_css "a[href='##{random_id}'] + .collapse##{random_id}"
+        end
+      end
+      context 'when an ID is supplied' do
+        let(:test_id) { 'test-id' }
+        it 'renders the combined components with the supplied :id' do
+          expect(capybara_component).to have_css "a[href='##{test_id}'] + .collapse##{test_id}"
+        end
+      end
+    end
+
+    context 'when a heading is not supplied' do
+      let(:test_heading) { nil }
+      it 'renders only the expected collapse component without a button' do
+        expect(collapse.render).to eq "<div class=\"collapse show\"></div>"
+      end
+    end
+  end
 end
