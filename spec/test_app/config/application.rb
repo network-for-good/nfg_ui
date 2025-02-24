0d2effe54e01d449893ad78cbe6247f58ff80c7d
Pavan Kumar K C <kcpavan@me.com>
2025-02-24 21:58:55 +0530

    [#NFG-1171] update rspec version

--- spec/test_app/config/application.rb
+++ spec/test_app/config/application.rb
@@ -20,7 +20,7 @@ require 'nfg_ui'
 module TestApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 5.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
